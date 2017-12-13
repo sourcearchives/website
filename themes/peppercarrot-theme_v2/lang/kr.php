65b4b22fe38df7716a34d5fa22ec6ead98289020
Jihoon Kim <imsesaok@tuta.io>
2017-12-13 16:19:17 +0900

    Modify Korean website translation

--- themes/peppercarrot-theme_v2/lang/kr.php
+++ themes/peppercarrot-theme_v2/lang/kr.php
@@ -3,7 +3,7 @@
 $LANG = array(
 
 # Global
-'TRANSLATED_BY' 		    => '번역자: <a href="https://www.facebook.com/hongpyo.jang">Remerer Jang</a>',
+'TRANSLATED_BY' 		    => '번역자: <a href="https://www.facebook.com/hongpyo.jang">Remerer Jang</a>, <a href="https://keybase.io/qtwyeuritoiy">Jihoon Kim</a>',
 'LANGUAGE_NAME' 			=> '한국어',
 'LANGUAGE_ISO_CODE_2_LETTER'=> 'ko',
 # http://www.w3schools.com/tags/ref_language_codes.asp --> full list
@@ -11,231 +11,231 @@ $LANG = array(
 ################################################################################
 # Header HTML infos for search engine and title in tab:
 'PEPPERCARROT_VEGETABLE'    =>  '후추와 당근', // litteral translation of spices + vegetable
-'Website_DESCRIPTION'       =>  '후추&amp;당근의 공식 홈페이지, 무료 그리고 오픈소스 웹툰의 어린마녀 후추와 그녀의 고양이 당근. 그들이 사는 포션, 마법 그리고 크리쳐들의 판타지 세계.',
-'SUBTITLE'                  =>  '웹툰작가 David Revoy',
+'Website_DESCRIPTION'       =>  '어린 마녀 후추와 그녀의 고양이 당근을 주인공으로 하는 무료 오픈소스 만화, 후추와 당근의 공식 홈페이지입니다. 이들은 상상의 포션과, 마법 그리고 생물의 세계에서 살아갑니다.',
+'SUBTITLE'                  =>  'David Revoy의 공개 만화',
 
 ################################################################################
 # Top menu website:
 'HOMEPAGE'      =>  '홈',
-'WEBCOMICS'     =>  '웹툰',
+'WEBCOMICS'     =>  '만화',
 'BLOG'          =>  '블로그',
 'PHILOSOPHY'    =>  '철학',
 'CONTRIBUTE'    =>  '참여',
 'COMMUNITY'     =>  '커뮤니티',
 'WIKI'          =>  '위키',
-'SOURCES'       =>  '소스파일',
+'SOURCES'       =>  '소스',
 'AUTHOR'        =>  '작가',
-'FOLLOW'        =>  '후추&amp;당근을 팔로우하기：',
+'FOLLOW'        =>  '후추와 당근 팔로우하기：',
 
 ################################################################################
 # Top and bottom translation panel
-'ADD_TRANSLATION'       => '번역추가',
-'CORRECTIONS' 			=> '수정제안',
+'ADD_TRANSLATION'       => '번역 추가',
+'CORRECTIONS' 			=> '수정 제안',
 
 ################################################################################
 # Page : Homepage
 'HOMEPAGE_BIG_TEXT'		    =>  '
-    무료 그리고 오픈소스인 이 웹툰은<br/>
-    만화책 시장을 바꾸기 위해<br/>
-    이곳에서 직접 후원할 수 있습니다!<br/>
+    무료이자 오픈소스인 이 만화는<br/>
+    독자들에게 작접 후원을 받아<br/>
+    만화책 시장을 바꾸고자 합니다. <br/>
 ',
-'HOMEPAGE_PATREON_INFO'     =>  '회차당 단돈 1달러면, Patreon에서 오늘 후원이이 되어보세요.',
-'HOMEPAGE_MOREINFO_BUTTON'  =>  '더 많은 내용',
-'HOMEPAGE_PATREON_BUTTON'   =>  '후추&amp;당근을 Patreon에서 후원하실 수 있습니다',
-'HOMEPAGE_PATREON_BOX'      =>  '후추&amp;당근을 이 곳에서 후원할 수 있습니다:',
+'HOMEPAGE_PATREON_INFO'     =>  '에피소드당 단돈 1달러(천 원)로 오늘 Patreon에서 후원자가 되어보세요.',
+'HOMEPAGE_MOREINFO_BUTTON'  =>  '더 보기',
+'HOMEPAGE_PATREON_BUTTON'   =>  '후추와 당근을 Patreon에서 후원하실 수 있습니다',
+'HOMEPAGE_PATREON_BOX'      =>  '후추와 당근을 이 곳에서 후원할 수 있습니다:',
 'HOMEPAGE_LAST_EPISODE'     =>  '최근 화：',
 'HOMEPAGE_NEWS_UPDATE'      =>  '뉴스와 업데이트：',
-'HOMEPAGE_MOREPOSTS_BUTTON' =>  '더 많은 게시물',
+'HOMEPAGE_MOREPOSTS_BUTTON' =>  '게시물 더 보기',
 'HOMEPAGE_MAINSERVICE_LINK' =>  'https://www.patreon.com/davidrevoy',
 'HOMEPAGE_SUPPORTED_BY'     =>  '후원자들.',
-'HOMEPAGE_ALTERNATIVES'     =>  '또 다른 방법:',
+'HOMEPAGE_ALTERNATIVES'     =>  '기타 방법:',
 
 ################################################################################
 # Page : Webcomics
-'WEBCOMIC_EPISODE'		    =>  '웹툰목록',
-'WEBCOMIC_MAKINGOF'		    =>  '제작후기',
+'WEBCOMIC_EPISODE'		    =>  '만화 목록',
+'WEBCOMIC_MAKINGOF'		    =>  '제작 과정',
 'WEBCOMIC_MAKINGOF_DESCRIPTION' =>  '
-    <p>제작후기와<a href="http://www.davidrevoy.com/categorie5/tutorials">습작들</a>은 특별히 제<a +href="https://www.patreon.com/davidrevoy">Patreons 으로 후원됩니다. &#39;heroes&#39;</a></p>
+    <p>제작후기와<a href="http://www.davidrevoy.com/categorie5/tutorials">그림 튜토리얼</a>은 특별히 제<a +href="https://www.patreon.com/davidrevoy">Patreon과 영웅들</a>이 후원합니다.</p>
 ',
-'WEBCOMIC_ARTWORK'		    =>  '아트워크',
+'WEBCOMIC_ARTWORK'		    =>  '그림 갤러리',
 'WEBCOMIC_SKETCHES'		    =>  '스케치',
 
 ################################################################################
 # Page : Blog
 # An empty one in English (no need to warn user the content is english only here) :
-# 'LIMITATIONS' 	=> '이 페이지는 영문에서만 사용할 수 있습니다',
+ 'LIMITATIONS' 	=> '이 페이지는 영문으로만 제공됩니다',
 
 ################################################################################
 # Page: Philosophy
 'PHILOSOPHY_TOP'    =>  '
  <h2>후원자로부터의 지원</h2>
     
-    <p>후추&amp;당근 프로젝트는 전 세계의 후원자들에게만 지원받고 있습니다.<br/>
-    각 후원자는 새 에피소드가 게시 될 때마다 약간의 돈을 보내고, 새 에피소드의 마지막에 크레딧을 받습니다.<br/>
-    이 시스템 덕분에 후추&amp;당근은 스스로 자립할 수 있고, 광고나 마케팅에 의해 더럽혀지지 않아도 됩니다.</p>
+    <p>후추와 당근 프로젝트는 전 세계의 후원자들이 지원하고 있습니다.<br/>
+    각 후원자는 새 에피소드가 게시 될 때마다 소정의 돈을 보내고, 새 에피소드의 마지막에 감사의 말을 받습니다.<br/>
+    이 시스템 덕분에 후추와 당근은 스스로 자립할 수 있고, 광고나 마케팅에 의존할 팔요가 없습니다.</p>
     
 ',    
 'PHILOSOPHY_BOTTOM'    =>  '
-    <img alt="illustration representing patronage" src="data/images/static/2015-02-09_philosophy_01-support.jpg">
+    <img alt="후원을 나타내는 삽화" src="data/images/static/2015-02-09_philosophy_01-support.jpg">
 
-    <h2>100% 무료, 영원히, 과금장벽 없음</h2>
+    <h2>영원히 100% 무료, 과금장벽 없음</h2>
     
-    <p>이 홈페이지의 제가 만든 후추&amp;당근의 모든 내용들은 자유롭게 사용 가능합니다.<br/>
-    저는 당신들을 똑같이 존중합니다：후원을 하거나, 안하거나요. <br/>모든 후원자들을 위한 특별한 보너스(자료)들은 이곳에도 게시됩니다. <br/>
-    후추&amp;당근은 새로운 컨텐츠에 접근하거나 구독할때, 절대 지불을 요구하지 않습니다.</p>
+    <p>이 홈페이지의 제가 만든 후추와 당근의 모든 내용들은 자유롭게 사용 가능합니다.<br/>
+    저는 당신들을 똑같이 존중합니다： 후원을 하거나, 안하거나요. <br/>후원자들을 위한 특별 자료들은 이곳에도 모두 게시됩니다.<br/>
+    후추와 당근은 절대 새로운 컨텐츠를 보기 위해 돈이나 구독을 요구하지 않습니다.</p>
     
-        <img alt="Illustration représentant les péages du net" src="data/images/static/2015-02-09_philosophy_03-paywall.jpg">
+        <img alt="과금장벽을 나타내는 삽화" src="data/images/static/2015-02-09_philosophy_03-paywall.jpg">
 
     <h2>오픈소스와 허용범위</h2>
     
-    <p>저는 모든 사람들에게 제 작품을 공유하고, 사용하고, 만들고 심지어 돈을 벌 수 있는 권리를 주고 싶습니다.<br/>
-    모든 페이지, 삽화 및 내용은 Gnu/Linux 의 무료 오픈소스 소프트웨어로 제작되었고, 모든 소스는 이 웹사이트의 &#39;소스&#39;탭에 있습니다.<br/>
+    <p>저는 모든 사람들에게 제 작품을 통해 공유하고, 사용하고, 만들고, 심지어 돈을 벌 수 있는 권리도 주려고 합니다.<br/>
+    모든 페이지, 삽화 및 내용은 GNU/Linux 에서 무료 오픈소스 소프트웨어로 제작되었고, 모든 소스는 이 웹사이트의 &#39;소스&#39;탭에 있습니다.<br/>
     상업적 사용, 번역, 팬아트, 인쇄, 영화, 비디오게임, 공유 그리고 재게시를 권장합니다.<br/>
-    당신은 단지 저작자(아티스트, 교정가, 번역가, 당신이 사용하려는 아트워크의 번역자)에게 적절한 크레딧을 주고 라이선스에 대한 링크를 제공하고 변경사항여부만 나타내면 됩니다. 당신은 합리적으로 예의를 갖춰 그렇게 해도 되지만, 다만 그것이 저작자들이 당신이 저작물을 어떻게 사용하든지 지지한다는 것을 의미하지는 않습니다.</p>
+    다만 저작자(아티스트, 교정사, 번역가, 당신이 사용하려는 그림의 번역가)를 명시해 주고, 라이선스에 대한 링크를 제공하고, 변경사항만 나타내면 됩니다. 상식적 테두리 안에서 어떻게든 표시하면 되지만, 저작자에게 허락을 받았다는 식으로 표시해서는 안 됩니다.</p>
     
     <div class="philobutton">
-        <a href="https://creativecommons.org/licenses/by/4.0/deed.zh" title="자세한 정보를 원하시면, Creative Commons Attribution 4.0 을 읽어주세요">
-            라이선스：Creative Commons Attribution 4.0
+        <a href="https://creativecommons.org/licenses/by/4.0/deed.ko" title="자세한 정보를 원하시면, 크리에이티브 커먼즈 저작자 표시 4.0을 읽어주세요">
+            라이선스：크리에이티브 커먼즈 저작자 표시 4.0
         </a>
     </div>
 
-        <img alt="Illustration représentant les médias open-sources" src="data/images/static/2015-02-09_philosophy_04-open-source.jpg">
+        <img alt="오픈소스 미디어를 나타내는 삽화" src="data/images/static/2015-02-09_philosophy_04-open-source.jpg">
 
     <h2>누구에게나, 어디서나 고품질의 엔터테인먼트 제공</h2>
     
-    <p>후추&amp;당근은 전 연령층을 위한 코미디/유머 웹툰입니다.<br/>
-    성인용 컨텐츠는 없으며, 폭력 또한 없습니다. <br/>오픈소스인 후추&amp;당근은 오픈소스문화가 얼마나 멋진지를 잘 보여주는 사례중 하나 입니다.<br/>
-    무료 및 오픈소스가 아마추어나 썩 좋지 못하다는 것을 의미하지는 않기 때문에 저는 품질에 초점을 맞추고 있습니다. <br/>전혀 다른것이죠!</p>
+    <p>후추와 당근은 전 연령층을 위한 코미디/유머 만화입니다.<br/>
+    선정성 및 폭력성을 띠지 않습니다. 무료 오픈소스인 후추와 당근은 자유 문화가 얼마나 멋진지를 잘 보여주는 훌륭한 사례중 하나입니다.<br/>
+    무료 및 오픈소스가 비전문적이거나 저질이라는 것을 의미하지는 않기 때문에, 저는 품질에 초점을 맞추고 있습니다. 오히려 그 반대라고 할 수 있죠.</p>
 
-        <img alt="Illustration pour tous" src="data/images/static/2015-02-09_philosophy_05-everyone.jpg">
+        <img alt="모두를 위한 삽화" src="data/images/static/2015-02-09_philosophy_05-everyone.jpg">
 
-    <h2>만화산업을 바꿉시다!</h2>
+    <h2>만화책 산업을 바꿉시다!</h2>
     
-    <p>예술가와 관객 사이의 중간단계가 적을수록 적은 비용을 지불하고, 더 많은 혜택을 얻을 수 있습니다. 당신은 저를 직접 후원합니다.<br/>
-    발행인/배급사/마케팅/상점은 저에게 후추&amp;당근을 그들이 시장을 바라보는 시선으로 바꿀 수 없습니다.<br/>
-    어째서 하나의 성공이 전체산업을 위협하는 신호가 될 수 없을까요? 우리 한번 해봅시다!</p>
+    <p>예술가와 독자 사이의 중간단계를 없애면 여러분은 비용을 절약하고 저는 더 많은 이득을 봅니다. 여러분은 저를 직접 후원합니다.<br/>
+    어떤 출판사/배급사/마케팅/상점도 후추와 당근을 그들이 원하는 &#39;시장&#39;에 맞게 바꾸도록 할 수 없습니다.<br/>
+    어째서 하나의 성공이 전체 산업을 위협하는 신호탄이 될 수 없을까요? 기대해 보자고요...</p>
     
 
-        <img alt="Illustration: comic book industry VS patreon support" src="data/images/static/2015-02-09_philosophy_06-industry-change.jpg">
+        <img alt="삽화: 기존 만화책 산업구조와 후원자(Patreon) 구조 비교" src="data/images/static/2015-02-09_philosophy_06-industry-change.jpg">
    
     <div class="philobutton">
-        <a class="h4" href="https://www.patreon.com/davidrevoy" title="회차당 단돈 $1로, 후추&amp;당근의 새 후원자가 되어주세요">
-            help me boost Pepper&amp;Carrot production today.
+        <a class="h4" href="https://www.patreon.com/davidrevoy" title="회차당 단돈 $1로, 후추와 당근의 새 후원자가 되어주세요">
+            오늘 후추와 당근의 제작에 힘을 보태 주세요.
         </a>
     </div>
 
     <p>읽어주셔서 감사합니다!<br/>
-    David Revoy</p>
+    -David Revoy</p>
  
  ',
  
 ################################################################################
 # Page : Contribute 
-'CONTRIBUTE_TITLE'		=> '기부',
+'CONTRIBUTE_TITLE'		=> '참여',
 'CONTRIBUTE_TOP'		=> '
-    <p><a href="?static6/sources" title="Sources page">오픈소스</a> 와 <a href="http://creativecommons.org/licenses/by/4.0/">
-    크리에이티브 커먼즈 라이선스</a> 덕분에 후추&amp;당근을 많은 방법으로 후원할 수 있습니다:</p>
+    <p><a href="?static6/sources" title="소스 페이지">오픈소스</a> 와 <a href="http://creativecommons.org/licenses/by/4.0/">
+    크리에이티브 커먼즈 라이선스</a> 덕분에 후추와 당근을 많은 방법으로 후원할 수 있습니다:</p>
 ',
 'CONTRIBUTE_DONATION'		=> '           
     <h2>후원 및 기부</h2> 
     
-    <p>저는 <a href="?static2/philosophy" title="Philosophie">철학</a>페이지에서 후원과 관련된 거의 모든것을 설명하고 있습니다.<br/>
-    <a href="https://www.patreon.com/davidrevoy">Patreon</a>에서 후추&amp;당근을 매 회차당 단돈 1$로 쉽게 후원하실 수 있습니다.<br/>
-    Patreon에서는 전 세계의 신용카드를 모두 사용할 수 있으며 Paypal계정으로도 결제할 수 있습니다.<br/>
+    <p>저는 <a href="?static2/philosophy" title="철학">철학</a> 페이지에서 후원과 관련된 거의 모든 것을 설명하고 있습니다.<br/>
+    <a href="https://www.patreon.com/davidrevoy">Patreon</a>에서 후추와 당근을 매 회차당 단돈 1$로 쉽게 후원하실 수 있습니다.<br/>
+    Patreon에서는 전 세계 신용카드를 모두 사용할 수 있으며, Paypal 계정으로도 결제할 수 있습니다.<br/>
     </p>
 ',
 'CONTRIBUTE_FANART'	    => '
     <h2>팬아트</h2>
 
-    <p>후추&amp;당근은 팬아트에게 열려있습니다：이모티콘, 대본，팬픽, 3D모델링 등...저에게 보내주세요 (<a href="mailto:info@davidrevoy.com">info@davidrevoy.com</a>，혹은 제 SNS를 통해 말해주세요) 그러면 팬아트갤러리에 게시해 드릴 수 있습니다 :</p>
+    <p>후추와 당근은 팬아트에 열려있습니다：그림, 대본, 조각, 3D 모델, 팬픽 등. 저에게 보내주시면(<a href="mailto:info@davidrevoy.com">info@davidrevoy.com</a>，아니면 SNS에서 절 불러주세요.) 팬아트 갤러리에 게시해 드릴 수 있습니다 :</p>
 ',
 'CONTRIBUTE_DERIVATIONS' => '
-    <h2>2차 창작물</h2>
+    <h2>2차 창작물:</h2>
 
-    <p>후추&amp;당근은 많은 부가상품을 만들어 내는것을 허락합니다. 왜 이런 재밌는 일에 참여하지 않는거죠?</p>
+    <p>다양한 창작물과 제품에 후추와 당근을 이용할 수 있습니다. 기존 프로젝트에 참가하거나 직접 만들어 보지 않으실래요?</p>
 ',
 
 'CONTRIBUTE_TRANSLATION'	    => '
     <h2>번역 및 수정</h2>
 
-    <p>후추&amp;당근 웹사이트는 다국어로 되어 있으며, 모든언어 (사멸어 혹은 가상어, 인공어포함)를 허용합니다. 이 페이지의 출처는 번역할 수 있습니다. 번역 추가 방법에 대한 자세한 내용은<a href="index.php?fr/article267/translation-tutorial">참조 안내서</a>를 확인하세요.</p>
+    <p>후추와 당근 웹사이트는 다국어로 사용할 수 있도록 되어 있으며 어떤 언어든(사멸어 혹은 가상어 포함) 허용합니다. 이 페이지의 소스는 번역할 수 있도록 항상 준비되어 있습니다. 번역 방법에 대한 자세한 내용은<a href="index.php?fr/article267/translation-tutorial">참조 안내서</a>를 확인하세요.</p>
 ',
 
-'CONTRIBUTE_SOCIAL'	    => '소셜네트워크:',
+'CONTRIBUTE_SOCIAL'	    => '소셜 네트워크:',
  
 'CONTRIBUTE_PRESS'	    => '
     <h2>출판:</h2>
 
-    <p>후추&당근의 발행인이 되어주세요! 기사를 작성하고, 웹사이트에 게시물을 올리고, 당신이 좋아하는 SNS에서 커뮤니티를 구축하고 내용을 공유하세요. <a href="?static6/sources" title="Sources page">프레스 킷</a>은 "소스파일" 탭에서 받으실 수 있습니다..</p>
+    <p>후추&당근을 출판해 주세요! 기사를 작성하고, 웹사이트에 게시물을 올리고, 당신이 즐겨 찾는 SNS에서 커뮤니티를 구축하고 내용을 공유하세요. <a href="?static6/sources" title="소스 페이지">보도 자료</a>는 "소스" 탭에서 받으실 수 있습니다..</p>
 ',
 
 'CONTRIBUTE_OTHER'	    => '
-    <h2>기타 다른 후원방법들&nbsp;.&nbsp;.&nbsp;.&nbsp;</h2>
+    <h2>기타 후원 방법들&nbsp;.&nbsp;.&nbsp;.&nbsp;</h2>
 
     <p>이 외에 여러 방법으로 후원하실 수 있습니다:<br/>
-        <b>개발자:</b> 후추&amp;당근을 휴대기기에서 읽을 수 있는 어플리케이션을 만듭니다.<br/>
-        <b>음악가:</b> 후추&amp;당근을 위한 음악테마를 만듭니다.<br/>
-        <b>작가:</b> 후추&amp;당근의 줄거리를 제안합니다.<br/>
-        <b>기자:</b> 후추&amp;당근의 정보를 여러미디어에 투고합니다.(정간물, TV, 등..)<br/>
-        <b>인쇄업:</b> 후추&amp;당근의 포스터나 굿즈를 인쇄하세요.<br/>
+        <b>개발자:</b> 후추와 당근을 휴대기기에서 읽을 수 있는 어플리케이션을 만듭니다.<br/>
+        <b>음악가:</b> 후추와 당근을 위한 음악 테마를 만듭니다.<br/>
+        <b>작가:</b> 후추와 당근의 시나리오를 제안합니다.<br/>
+        <b>기자:</b> 후추와 당근의 정보를 여러 미디어에 투고합니다.(정간물, TV, 등..)<br/>
+        <b>인쇄업:</b> 후추와 당근의 포스터나 굿즈를 인쇄하세요.<br/>
     </p>
 ',
 
 'CONTRIBUTE_IRC'	    => '
     <h2>IRC 채널:</h2>
 
-    <p>후추&amp;당근에 대해 이야기하고, 토론하세요. 저는 유럽 낮시간대에 종종 참여할 것입니다.(닉네임:deevad)<br/>
+    <p>후추와 당근에 대해 이야기하고 토론하세요. 저는 유럽 낮 시간대에 종종 참여할 겁니다.(닉네임: deevad)<br/>
 ',
 
 ################################################################################
 # Page : Sources
-'SOURCES_TITLE'		    => '소스파일',
+'SOURCES_TITLE'		    => '소스',
 'SOURCES_TOP'	=> '
-    <p><b>소스파일 다운로드센터에 오신것을 환영합니다!</b><br/>
-    후추&amp;당근의 소스파일과 그 외 다른 것들을 찾으실 수 있습니다.<br/>
-    모든 디지털 그림파일은 최신버전의 <a href="https://krita.org/">Krita</a>와 호환됩니다.</p>
+    <p><b>소스 다운로드 센터에 오신것을 환영합니다!</b><br/>
+    후추와 당근의 소스와 그 외 다른 것들을 찾으실 수 있습니다.<br/>
+    모든 디지털 그림파일은 최신 버전의 <a href="https://krita.org/">Krita</a>와 호환됩니다.</p>
 ',
 'SOURCES_BOTTOM'	=> '
     <p>이 파일들을 다운로드하고 사용함으로써 당신은<br/>
-    <a href="http://creativecommons.org/licenses/by/4.0/deed.zh">크리에이티브 커먼즈 라이선스</a>를 동의하고 준수해야 합니다.<br/>
-    각 프로젝트의 README파일에 자세한 내용이 있습니다.<br/></p>
+    <a href="http://creativecommons.org/licenses/by/4.0/deed.ko">크리에이티브 커먼즈 라이선스</a>를 준수함에 동의합니다.<br/>
+    자세한 사항은 각 프로젝트의 README 파일에 있습니다.<br/></p>
   
     <p><b>번역, 스크립트, 웹 사이트, 그래픽 등을 위한 코드저장소：</b><br/></p>
 ',
 'SOURCE_COVER'   =>  '표지',
-'SOURCE_KRITA'   =>  'Krita소스파일',
+'SOURCE_KRITA'   =>  'Krita 소스',
 'SOURCE_TRANSLATOR'=>  '번역 팩',
 'SOURCE_WEB'      =>  '웹 <span class="sourceinfos"> 90ppi，저용량 jpg</span>',
 'SOURCE_PRINT'        =>  '인쇄 <span class="sourceinfos"> 300ppi，무손실</span>',
-'SOURCE_MONTAGE'   =>  '통합 <span class="sourceinfos"> 단일페이지의 모든내용</span>',
+'SOURCE_MONTAGE'   =>  '통합 <span class="sourceinfos"> 전체 내용을 한 페이지에 수록</span>',
 
 ################################################################################
 # Page : Author
-'AUTHOR_TITLE'		    => '작가 David Revoy',
+'AUTHOR_TITLE'		    => 'David Revoy 소개',
 'AUTHOR_BIO'	        => '
-    <p>안녕하세요，제 이름은 David Revoy 이고 1981년생인 프랑스 예술가입니다. 저는 드로잉, 페인팅, 고양이, 컴퓨터, Gnu/Linux 오픈소스문화, 인터넷, 고전학교 RPG 비디오게임, 고전 만화와 애니메이션, 전통예술, 일본문화, 판타지를 좋아해서 독학하고 있습니다.<br />
+    <p>안녕하세요，제 이름은 David Revoy이고, 1981년생 프랑스 예술가입니다. 저는 드로잉, 페인팅, 고양이, 컴퓨터, Gnu/Linux 오픈소스문화, 인터넷, 고전학교 RPG 비디오게임, 고전 만화와 애니메이션, 전통예술, 일본문화, 판타지를 좋아해서 독학하고 있습니다.<br />
     <br />
-    프리랜서로 디지털페인팅, 강의, 컨셉아트, 일러스트레이션, 아트디렉션분야에서 10년 이상 경력을 쌓은 후, 저는 제 프로젝트를 시작하기로 결정했습니다. 그리고 마침내 저는 모든열정을 하나로 모을 수 있는 방법을 찾앚고, 그 결과가 후추&amp;당근 입니다.</p>
+    프리랜서로 디지털 페인팅, 강의, 컨셉 아트, 삽화, 미술 감독 분야에서 10년 이상 경력을 쌓은 후, 저는 제 프로젝트를 시작하기로 결정했습니다. 그리고 마침내 저는 모든 열정을 하나로 모을 수 있는 방법을 찾았고, 그 결과가 후추와 당근입니다.</p>
     
-    <p>나의 포트폴리오：</p>
+    <p>저의 포트폴리오：</p>
 ',
 'AUTHOR_TODO_DREAM'	    => '
-    <h3>꿈 실천목록：</h3>
+    <h3>꿈 실천 목록：</h3>
 
     <p>
-    ☐ 일본 만화 전시회에서 후추&amp;당근에 대해 연설하기<br/>
-    ☐ 게임패드로 후추&amp;당근 게임 플레이하기<br/>
+    ☐ 일본 만화 전시회에서 후추와 당근에 대해 연설하기<br/>
+    ☐ 게임패드로 후추와 당근 게임을 하기<br/>
     ☐ 갤러리에 100개의 팬아트받기<br/>
-    ☑ <a href="http://www.peppercarrot.com/en/article376/dream-to-do-list-a-wikipedia-page">위키백과페이지 만들기</a><br/>
-    ☑ <a href="http://www.peppercarrot.com/article387/cosplay-by-maria-and-ekaterina">후추 코스프레사진 받기</a><br/>
-    ☑ <a href="http://www.peppercarrot.com/article304/dream-to-do-list-a-cat-named-carrot">이름이 당근인 빨간 고양이사진 받기</a><br/>
-    ☑ <a href="http://www.peppercarrot.com/article302/peppercarrot-has-over-500-supporters">Patreon으로 500명의 후원인 모집</a><br/>
+    ☑ <a href="http://www.peppercarrot.com/en/article376/dream-to-do-list-a-wikipedia-page">위키백과에 수록되기</a><br/>
+    ☑ <a href="http://www.peppercarrot.com/article387/cosplay-by-maria-and-ekaterina">후추 코스프레 사진 받기</a><br/>
+    ☑ <a href="http://www.peppercarrot.com/article304/dream-to-do-list-a-cat-named-carrot">이름이 당근인 빨간 고양이 사진 받기</a><br/>
+    ☑ <a href="http://www.peppercarrot.com/article302/peppercarrot-has-over-500-supporters">Patreon으로 500명의 후원자 모으기</a><br/>
     ☐ 100화 연재!<br/>
     </p>
 ',
 
-'AUTHOR_CARREER_TITLE'				    => '내 7개의 경력구슬：',
-'AUTHOR_CARREER_BUBBLE_DESCRIPTIONS'    => '<p>(그림내용 번역)</p>',
+'AUTHOR_CARREER_TITLE'				    => '제 7개의 경력 구슬：',
+'AUTHOR_CARREER_BUBBLE_DESCRIPTIONS'    => '<p></p>',
 
 ################################################################################
 # Page: Wiki
@@ -244,10 +244,10 @@ $LANG = array(
 ################################################################################
 # Website General : Footer
 'FOOTER_CONTENT'		=> '
-    <p>웹툰과 작품 그리고 모든 글은 <a href="https://creativecommons.org/licenses/by/4.0/deed.zh" title="자세한 내용을 원하신다면, Creative Commons Attribution 4.0 을 참조하십시오">Creative Commons Attribution 4.0</a>라이선스를 따르며, 이 외의 경우에는 홈페이지에 별도로 표기됩니다.<br/>
+    <p>만화과 작품 그리고 모든 글은 별도로 명시되지 않은 경우 <a href="https://creativecommons.org/licenses/by/4.0/deed.ko" title="자세한 내용을 원하신다면, 크리에이티브 커먼즈 저작자 표시 4.0 을 참조하십시오">크리에이티브 커먼즈 저작자 표시 4.0</a>라이선스를 따릅니다.<br/>
     작가：David Revoy，www.davidrevoy.com 더 많은 정보를 원하신다면，<a href="mailto:info@davidrevoy.com">info@davidrevoy.com</a>로 연락주세요.</p>
     
-    <p>이 웹사이트는 <a href="http://www.pluxml.org" title="PlXml">PluXml</a>에서 제공합니다</p> 
+    <p>이 웹사이트는 <a href="http://www.pluxml.org" title="PluXml">PluXml</a>에서 제공합니다</p> 
 ',
 
 ################################################################################
