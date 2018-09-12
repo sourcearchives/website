1b10dd0e2d5537d89261d34ae8f80aee69497b45
David Revoy <info@davidrevoy.com>
2018-09-12 16:28:05 +0200

    [homepage] Add a new "Fan-art" box
    This one is possible with the new caching system: the code to list
    all jpg was indeed a bit too consuming in resource for the homepage

--- themes/peppercarrot-theme_v2/static-homepage.php
+++ themes/peppercarrot-theme_v2/static-homepage.php
@@ -28,7 +28,8 @@ $lang = $plxShow->defaultLang($echo);
           </div>
         </div>
       </section>  
-
+      
+      <?php # ===========  Latest episode box ================ ?>
       <section class="col sml-12 med-4 lrg-4">
         <div class="homebox">
           <h2><?php $plxShow->lang('HOMEPAGE_LAST_EPISODE') ?></h2>
@@ -38,7 +39,7 @@ $lang = $plxShow->defaultLang($echo);
             <div class="lastep">
               <figure>
                 <a href="#art_url" title="#art_title">
-                  <img src="plugins/vignette/plxthumbnailer.php?src=#episode_vignette&amp;w=630&amp;h=550&amp;s=1&amp;q=65" alt="#art_title" title="#art_title, click to read" >
+                  <img src="plugins/vignette/plxthumbnailer.php?src=#episode_vignette&amp;w=630&amp;h=545&amp;s=1&amp;q=65" alt="#art_title" title="#art_title, click to read" >
                 </a>
                 <figcaption><a href="#art_url" title="#art_title"><span class="detail">#art_date#art_nbcoms</span></a></figcaption>
               </figure>
@@ -53,6 +54,7 @@ $lang = $plxShow->defaultLang($echo);
         </div>
       </section>
       
+      <?php # ===========  News and updates box ================ ?>
       <section class="col sml-12 med-8 lrg-8">
         <div class="homebox news">
           <h2><?php $plxShow->lang('HOMEPAGE_NEWS_UPDATE') ?></h2>
@@ -69,7 +71,7 @@ $lang = $plxShow->defaultLang($echo);
                 </figure>
                 </div>
               </div>
-              ',6,'001|004|005|006|007|008|009|010|011|012|013', "...", "rsort"))); 
+              ',6,'001|005|006|007|008|009|010|011|012|013', "...", "rsort"))); 
             ?>
           </div>
             <div style="clear:both;"></div>
@@ -78,6 +80,72 @@ $lang = $plxShow->defaultLang($echo);
           </div>
         </div>
       </section>
+
+      <div style="clear:both; padding-top: 1rem;"></div>
+
+      <?php # ===========  Fan Art box ================ ?>
+      <section class="col sml-12 med-4 lrg-4">
+        <div class="homebox news">
+          <h2><?php $plxShow->lang('HOMEPAGE_FANART') ?></h2>
+          <div class="homecontent" style="margin-right: -1rem;">
+            <?php
+            #variables:
+            $fanartcounter = 0;
+            $pathartworks = '0_sources/0ther/fan-art';
+            $hide = array('.', '..');
+            $mainfolders = array_diff(scandir($pathartworks), $hide);
+            $search = glob($pathartworks."/*.jpg");
+            rsort($search);
+            # we loop on found episodes
+            if (!empty($search)){ 
+              foreach ($search as $filepath) {
+                if ( $fanartcounter < 4 ){
+                  # filename extraction
+                  $fileweight = (filesize($filepath) / 1024) / 1024;
+                  $fanartcounter = $fanartcounter + 1;
+                  $filename = basename($filepath);
+                  $fullpath = dirname($filepath);
+                  $dateextracted = substr($filename,0,10).'';
+                  $dateextracted = str_replace('-', '&#47;', $dateextracted);
+                  $filenameclean = preg_replace('/\\.[^.\\s]{3,4}$/', '', $filename);
+                  $filenameclean = substr($filenameclean, 11); // rm iso date
+                  $filenameclean = str_replace('_', ' ', $filenameclean);
+                  $filenameclean = str_replace('-', ' ', $filenameclean);
+                  $details = strstr($filenameclean, 'by');
+                  $title = stristr($filenameclean, 'by', true);
+                  $filenameclean = str_replace('featured', '', $filenameclean);
+                  $filenamezip = str_replace('jpg', 'zip', $filename);      
+                  echo '<div class="col sml-3 med-6 lrg-6" style="padding:0 1rem 0 0; margin: 0 0 1rem 0;">
+                          <div class="homethumbnail">
+                            <figure>
+                            <a href="0_sources/0ther/fan-art/'.$filename.'" title="'.$filenameclean.', '.$dateextracted.'">
+                              <img src="plugins/vignette/plxthumbnailer.php?src=0_sources/0ther/fan-art/'.$filename.'&amp;w=130&amp;h=110&amp;s=1&amp;q=65&amp;a=t" alt="'.$filenameclean.'" title="'.$filenameclean.', '.$dateextracted.'" >
+                            </a>
+                            <figcaption><a href="0_sources/0ther/fan-art/'.$filename.'" title="'.$filenameclean.', '.$dateextracted.'">'.$title.' <br/><span class="detail">'.$details.' '.$dateextracted.'</span></a></figcaption>
+                          </figure>
+                          </div>
+                        </div>
+                  ';
+                }
+              }
+            }
+            $fanartcounter = 0;
+            $search = glob($pathartworks."/*.jpg");
+            if (!empty($search)){ 
+              foreach ($search as $filepath) {
+                $fanartcounter = $fanartcounter + 1;
+              }
+            }?>  
+            <div style="clear:both;"></div>
+            <div class="moreposts" style="margin-top: 0.3rem;">
+              <a  class="button blue" href="<?php $plxShow->urlRewrite('?static10/fanart-gallery') ?>" title="Go to the blog page">
+                <?php $plxShow->lang('HOMEPAGE_MOREPOSTS_BUTTON') ?> (<?php $plxShow->lang('HOMEPAGE_FANART') ?>) &nbsp;&nbsp;<img class="svg" src="themes/peppercarrot-theme_v2/ico/go.svg" alt="→"/>
+              </a>
+            </div>
+          </div>
+        </div>
+      </section>
+
 
 	</main>
           <?php include(dirname(__FILE__).'/share-static.php'); ?>  <br/>
