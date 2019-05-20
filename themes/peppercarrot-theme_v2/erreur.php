c400471febd73222eff508a37191ce982c40c4d7
David Revoy <info@davidrevoy.com>
2019-05-20 23:37:49 +0200

    Add hints to the error page, and also clean-up the page.

--- themes/peppercarrot-theme_v2/erreur.php
+++ themes/peppercarrot-theme_v2/erreur.php
@@ -1,29 +1,23 @@
 <?php include(dirname(__FILE__).'/header.php'); ?>
 <div class="container">
 	<main class="main grid" role="main">
-
 		<section class="col sml-12">
 
       <div class="grid">
-        
         <div class="col sml-12 center" style="text-align:center; padding-top: 1rem;">
           <img src="0_sources/ep04_Stroke-of-genius/low-res/en_Pepper-and-Carrot_by-David-Revoy_E04P05.gif" />
           
           <div class="alert blue" style="margin: 0 auto; text-align:center; max-width: 910px; margin-bottom: 1.5rem;">
             <img src="themes/peppercarrot-theme_v2/ico/nfo.svg" alt=""/>
-            <strong>Error: Page not found.</strong><br/>
+            <strong>Error: Page not found.</strong><br/><br/> 
+            Hint: If you were following a link,<br/>
+            try the search field on the top <a href="https://www.davidrevoy.com/blog"><u>of my blog</u></a>,<br/> 
+            I rarely delete content.<br/>
           </div>
         </div>
-        
-        <div style="clear:both;"></div>
-
-        <br/><br/><br/><br/>
-
-        <footer class="grid text-center">
-        </footer>
+      </div>
 
 		</section>
-
 	</main>
 </div>
 <?php include(dirname(__FILE__).'/footer.php'); ?>
