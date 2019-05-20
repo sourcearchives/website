8168cbd187edd7e15265d2db432be05e06a94edb
David Revoy <info@davidrevoy.com>
2019-05-20 23:25:49 +0200

    Simplify Error page.

--- themes/peppercarrot-theme_v2/erreur.php
+++ themes/peppercarrot-theme_v2/erreur.php
@@ -12,21 +12,10 @@
           <div class="alert blue" style="margin: 0 auto; text-align:center; max-width: 910px; margin-bottom: 1.5rem;">
             <img src="themes/peppercarrot-theme_v2/ico/nfo.svg" alt=""/>
             <strong>Error: Page not found.</strong><br/>
-            But fear not! Carrot is diving into our database to show you alternatives:
           </div>
         </div>
         
         <div style="clear:both;"></div>
-        
-        <?php eval($plxShow->callHook("vignetteArtList", array('
-        <!-- #art_title -->
-        <figure class="thumbnail col sml-4 med-3 lrg-3">
-          <a href="#art_url" title="#art_title">
-            <img src="plugins/vignette/plxthumbnailer.php?src=#art_vignette&amp;w=370&amp;h=255&amp;s=1&amp;q=92" alt="#art_title" title="#art_title, click to read" >
-          </a>
-            <figcaption class="text-center"><a href="#art_url" title="#art_title"><strong>#category_list: </strong>#art_title<br/><span class="detail">#art_date#art_nbcoms</span></a></figcaption>
-        </figure>
-        ',96,'', "...", "rsort"))); ?>
 
         
       <section class="col sml-12 med-12 lrg-12 text-right">
