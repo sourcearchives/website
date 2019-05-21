44520500aaad58083038ba122d66f6139733230d
David Revoy <info@davidrevoy.com>
2019-05-21 17:20:25 +0200

    Fixes for the Fan-art page
    The page had various issue, mainly layout and rendering of the thumbnail
    and caption.

--- themes/peppercarrot-theme_v2/static-fanart-gallery.php
+++ themes/peppercarrot-theme_v2/static-fanart-gallery.php
@@ -1,23 +1,33 @@
 <?php include(dirname(__FILE__).'/header.php'); ?>
 <div class="container">
-	<main class="grid" role="main">
-        
+	<main class="main grid" role="main">
     <section class="col sml-12 med-12" style="padding: 0 0;">
       
 <?php 
-  $plxShow->lang('CONTRIBUTE_FANART');
-  $lang = $plxShow->getLang('LANGUAGE_ISO_CODE_2_LETTER');
-  $pathartworks = '0_sources/0ther/fan-art';
-  $search = glob($pathartworks."/*.jpg");
-   if (!empty($search)){ 
-    foreach ($search as $filepath) {
+# Setup
+$pathartworks = '0_sources/0ther/fan-art';
+$lang = $plxShow->getLang('LANGUAGE_ISO_CODE_2_LETTER');
+
+# Count fan-arts
+$fanartcounter = 0;
+$search = glob($pathartworks."/*.jpg");
+ if (!empty($search)){ 
+  foreach ($search as $filepath) {
     $fanartcounter = $fanartcounter + 1;
-    }
-   }
-  echo ''.$fanartcounter.' % <meter value="'.$fanartcounter.'" min="0" max="100"></meter> of my dream to receive 100 fan-arts !<br><br><br>';
-  #variables:
+  }
+}
+
+# Intro
+echo '<div class="grid"><br/>';
+echo '  <div class="col sml-6 sml-centered sml-text-center">';
+echo ''.$plxShow->Getlang(CONTRIBUTE_FANART).'';
+echo '('.$fanartcounter.' !) <br/><br/>';
+echo '  </div>';
+echo '</div>';
+
+#variables:
   $fanartcounter = 0;
-  $pathartworks = '0_sources/0ther/fan-art';
+
   $hide = array('.', '..');
   $mainfolders = array_diff(scandir($pathartworks), $hide);
 
@@ -33,13 +43,13 @@
       $dateextracted = substr($filename,0,10).'';
       $filenameclean = preg_replace('/\\.[^.\\s]{3,4}$/', '', $filename);
       $filenameclean = substr($filenameclean, 11); // rm iso date
+      $filenameclean = str_replace('_by', '</a><br/><span class="detail"> by', $filenameclean);
       $filenameclean = str_replace('_', ' ', $filenameclean);
       $filenameclean = str_replace('-', ' ', $filenameclean);
       $filenameclean = str_replace('featured', '', $filenameclean);
-      $filenameclean = str_replace('by', '</a><br/><span class="detail">by', $filenameclean);
       $filenamezip = str_replace('jpg', 'zip', $filename);
       echo '<figure class="thumbnail col sml-6 med-3 lrg-3">';
-      echo '<a href="0_sources/0ther/fan-art/'.$filename.'" ><img src="plugins/vignette/plxthumbnailer.php?src='.$filepath.'&amp;w=370&amp;h=370&amp;s=1&amp;q=92" alt="'.$filename.'" title="'.$filename.'" ></a><br/>';
+      echo '<a href="0_sources/0ther/fan-art/'.$filename.'" ><img src="plugins/vignette/plxthumbnailer.php?src='.$filepath.'&amp;w=370&amp;h=370&amp;&amp;zc=2s=1&amp;q=92" alt="'.$filename.'" title="'.$filename.'" ></a><br/>';
       echo '<figcaption class="text-center" >
       <a href="0_sources/0ther/fan-art/'.$filename.'" >
       '.$filenameclean.'
@@ -51,7 +61,6 @@
   }
 ?>
     </section>
-
 	</main>
 </div>
 <?php include(dirname(__FILE__).'/footer.php'); ?>
