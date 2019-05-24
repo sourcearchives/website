9a90b1dc726238992b6781a00c63c97d785e57d4
David Revoy <info@davidrevoy.com>
2019-05-24 17:39:19 +0200

    Fix the fake database in the repo to run newer website
    Without this; the website test.peppercarrot.com or any new
    install of the website repository would fails.

---
+++ .data/statiques/012.iban-and-mail-address.php
@@ -0,0 +1,27 @@
+<center>
+
+<h2>Donate via Bank Transfer</h2>
+   <p>I've set up a bank account to receive private donations from individuals, companies and other organisations.</p>
+   <p><b>Bank Account</b><br/>
+          IBAN - FR6230002040630000090557K69<br/>
+          RIB - 30002040630000090557K69<br/>
+          BIC - CRLYFRPP<br/>
+    </p>
+
+<h2>Donate directly in my mailbox</h2>
+   <p>If you want to send me presents, hardware or anything else you are welcome to send them to this address:<br/>
+   <br/>
+       David REVOY au Cabinet Dentaire<br/>
+       135 Avenue des Mourets <br/>
+       82000 MONTAUBAN<br/>
+       FRANCE<br/><br/>
+   <i>Note: This is the address of my dentist who has agreed to contribute<br/>
+        to the project by receiving mail and packages at his office and<br/>
+        redirecting them to me. This system preserves my privacy.<br/>
+    </i>
+    </p>
+
+<h2>Email contact</h2>
+    <p><a href="mailto:info@davidrevoy.com">info@davidrevoy.com</a></p>
+
+</center>
