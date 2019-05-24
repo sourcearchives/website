15ccacf6e4938a13e7daa1ac362468f175a17e98
David Revoy <info@davidrevoy.com>
2019-05-24 16:28:59 +0200

    Refactor the footer
    - remove old 'top' button
    - make it more compact
    - Add bigger social network here (no more on top)
    - make the theme a bit more flat

--- themes/peppercarrot-theme_v2/footer.php
+++ themes/peppercarrot-theme_v2/footer.php
@@ -1,33 +1,25 @@
 <?php if (!defined('PLX_ROOT')) exit; ?>
 <footer class="footer" role="contentinfo">
-    <div class="container">
-        <div class="grid">
-            
-            <div class="col sml-12 text-center"> 
+  <div class="container">
+      <div class="grid">
+          <div class="col sml-12 text-center"> 
             <p>
-              <a href="#top" title="Back to top"><img src="themes/peppercarrot-theme_v2/ico/top.svg" alt="+"/> TOP <img src="themes/peppercarrot-theme_v2/ico/top.svg" alt="+"/></a>
-                          
-              <div class="follow">
-                <?php $plxShow->lang('FOLLOW')?>&nbsp;<br/>
-                <a class="logo" href="https://www.facebook.com/pages/Pepper-Carrot/307677876068903" title="<?php $plxShow->lang('FOLLOW')?> Facebook"><img class="svg" src="themes/peppercarrot-theme_v2/ico/s_fb.svg" alt="Facebook"/></a>
-                <a class="logo" href="https://framapiaf.org/@davidrevoy" title="<?php $plxShow->lang('FOLLOW')?> Mastodon"><img class="svg" src="themes/peppercarrot-theme_v2/ico/s_mast.svg" alt="Mastodon"/></a>
-                <a class="logo" href="http://twitter.com/davidrevoy" title="<?php $plxShow->lang('FOLLOW')?> Twitter"><img class="svg" src="themes/peppercarrot-theme_v2/ico/s_tw.svg" alt="Twitter"/></a>
-                <a class="logo" href="<?php $plxShow->urlRewrite('feed.php?rss') ?>" title="RSS 2.0" target="blank"><img class="svg" src="themes/peppercarrot-theme_v2/ico/rss.svg" alt="Rss"/></a>
-              </div>
-              
-              <p><a href="<?php $plxShow->urlRewrite('?static13/terms-of-service-and-privacy') ?>" target="_blank">Terms of Services and Privacy (GDPR)</a> - <a href="<?php $plxShow->urlRewrite('?static14/documentation&page=409_Code_of_Conduct') ?>" title="link to the Code of Conduct in the Documentation" target="_blank">Code of Conduct (CoC)</a></p> 
-              
-              <p>
-                <p><?php $plxShow->lang('TRANSLATED_BY')?></p>
-                <?php $plxShow->lang('FOOTER_CONTENT') ?> 
-              
-                  
-              <br/>      
-            </div>
-
-            <br/>
-        </div>
-    </div>
+            <?php $plxShow->lang('FOLLOW')?>&nbsp;<br/>
+            <a class="logo" href="https://framapiaf.org/@davidrevoy" title="<?php $plxShow->lang('FOLLOW')?> Mastodon"><img class="svg" src="themes/peppercarrot-theme_v2/ico/s_mast.svg" alt="Mastodon"/></a>
+            <a class="logo" href="https://framasphere.org/people/f36d0ea092e50134ec422a0000053625" title="<?php $plxShow->lang('FOLLOW')?> Diaspora"><img class="svg" src="themes/peppercarrot-theme_v2/ico/s_dia.svg" alt="Diaspora"/></a>
+            <a class="logo" href="http://twitter.com/davidrevoy" title="<?php $plxShow->lang('FOLLOW')?> Twitter"><img class="svg" src="themes/peppercarrot-theme_v2/ico/s_tw.svg" alt="Twitter"/></a>      
+            <a class="logo" href="https://www.facebook.com/pages/Pepper-Carrot/307677876068903" title="<?php $plxShow->lang('FOLLOW')?> Facebook"><img class="svg" src="themes/peppercarrot-theme_v2/ico/s_fb.svg" alt="Facebook"/></a>              
+            <a class="logo" href="https://www.davidrevoy.com/static4/rss-options" title="RSS 2.0" target="blank"><img class="svg" src="themes/peppercarrot-theme_v2/ico/rss.svg" alt="Rss"/></a>
+            </p>
+            <?php $plxShow->lang('FOOTER_CONTENT') ?> 
+            <p>
+            <?php $plxShow->lang('TRANSLATED_BY')?>, <a href="<?php $plxShow->urlRewrite('?static13/terms-of-service-and-privacy') ?>" target="_blank">Terms of Services and Privacy.</a>
+            </p> 
+            <br/>      
+          </div>
+        <br/>
+      </div>
+  </div>
 </footer>
 
 </body>
