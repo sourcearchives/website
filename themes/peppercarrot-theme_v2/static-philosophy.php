1dea82e6a9738c8266e9aad76f447cd10b6a2bb4
David Revoy <info@davidrevoy.com>
2019-05-24 17:23:50 +0200

    Refactor the top part of Philosophy page
    No need to use this part of the website to write all the details
    about donation. A link is enough; donation should be ported to
    another page (TODO).

--- themes/peppercarrot-theme_v2/static-philosophy.php
+++ themes/peppercarrot-theme_v2/static-philosophy.php
@@ -3,29 +3,19 @@
   <main class="main grid" role="main">
 		<section class="col sml-12 med-12 lrg-10 sml-centered med-centered lrg-centered">
 
-      <div class="grid">
-
-      <div class="translabar col sml-12 med-12 lrg-12 sml-centered sml-text-center">
-        <ul class="menu" role="toolbar">
-          <?php eval($plxShow->callHook('MyMultiLingueStaticLang')) ?>
-          <li><a class="lang" href="<?php $plxShow->urlRewrite('?static14/documentation&page=010_Translate_the_comic') ?>"><img src="themes/peppercarrot-theme_v2/ico/add.svg" alt="+"/> <?php $plxShow->lang('ADD_TRANSLATION') ?></a></li>
-        </ul>
-      </div>   
-       
-       <article class="philosophy col sml-12 med-12 lrg-12 text-center" role="article">
-         <?php $plxShow->lang('PHILOSOPHY_TOP') ?>
-         <?php include(dirname(__FILE__).'/supportme.php'); ?>
-         <?php $plxShow->lang('PHILOSOPHY_BOTTOM') ?>
-       </article> 
-
-<!-- Footer infos -->
-<div style="clear:both;"></div>
-<footer class="col sml-12 med-12 lrg-12 text-center">
-  <?php include(dirname(__FILE__).'/share-static.php'); ?>          
-  <div class="col sml-12 text-center">
-    <br/><?php $plxShow->lang('TRANSLATED_BY') ?>
-  </div>
-</footer>
+        <div class="grid">
+        <div class="translabar col sml-12 med-12 lrg-12 sml-centered sml-text-center">
+          <ul class="menu" role="toolbar">
+            <?php eval($plxShow->callHook('MyMultiLingueStaticLang')) ?>
+            <li><a class="lang" href="<?php $plxShow->urlRewrite('?static14/documentation&page=010_Translate_the_comic') ?>"><img src="themes/peppercarrot-theme_v2/ico/add.svg" alt="+"/> <?php $plxShow->lang('ADD_TRANSLATION') ?></a></li>
+          </ul>
+        </div>   
+        <article class="philosophy col sml-12 med-12 lrg-12 text-center" role="article">
+          <?php $plxShow->lang('PHILOSOPHY_TOP') ?>
+          <?php echo '<a href="TODO-DONATEPAGE">'.$plxShow->Getlang(PATRONAGE_BUTTON).'</a>.'; ?>
+          <?php $plxShow->lang('PHILOSOPHY_BOTTOM') ?>
+        </article> 
+        <div style="clear:both;"></div>
 
 	</main>
 </div>
