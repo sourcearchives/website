ae8e2cfb6ab18805721722201f8a79aedaa3399b
David Revoy <info@davidrevoy.com>
2019-05-31 21:30:12 +0200

    Merge 'Wiki' and 'Documentation' generator.
    - Both needs the same markdown structure/repo

--- themes/peppercarrot-theme_v2/static-documentation.php
+++ themes/peppercarrot-theme_v2/static-documentation.php
@@ -1,117 +1,13 @@
 <?php
-$lang = $plxShow->defaultLang($echo);
-
-// get new variable 'page'
-$docpage = htmlspecialchars($_GET["page"]);
-
-// Security, remove all special characters except A-Z, a-z, 0-9, dots, hyphens, underscore before interpreting something. 
-$docpage = preg_replace('/[^A-Za-z0-9\._-]/', '', $docpage);
-
-// reset status for active pages
-$status="";
-$statushome="";
-
-  if(isset($_GET['page']))
-  {
-    // page found
-  }else{
-    // no page found, we propose homepage by default and active status
-    // new homepage is README.md
-    $docpage = "README";
-    $statushome="active";
-  }
-
-// add libraries
+include(dirname(__FILE__).'/header.php');
 include(dirname(__FILE__).'/lib-parsedown.php');
-include(dirname(__FILE__).'/header.php'); 
+
+// Setup
+$repositoryURL = "https://framagit.org/peppercarrot/documentation";
+$currentpage = "?static14/documentation";
+$datapath = "data/documentation/";
+
+include(dirname(__FILE__).'/lib-wiki.php');
+
+include(dirname(__FILE__).'/footer.php'); 
 ?>
-
-<div class="container">
-	<main class="main grid" role="main">
-
-    <aside class="aside col sml-12 med-4" role="complementary">
-      <section class="docmenuitemmenu col sml-12 med-12 lrg-12" style="padding:0 0;">
-        <a class="docmenuitem <?php echo $statushome; ?>" href="<?php $plxShow->urlRewrite('?static14/documentation'); ?> " title="">
-          Introduction
-        </a>
-        <?php
-        // dynamic menu generation for sidebar
-        # we scan all markdown in folder
-        $search = glob("data/documentation/*.md");
-        # we loop on found files
-        if (!empty($search)){ 
-          foreach ($search as $docfile) {
-            // clean path to filename only
-            $docfile = basename($docfile);
-            // _Footer and _Sidebar are special page, they start with '_' exclude them
-            if (substr($docfile, 0, 1) === '_') {
-              // page starting with '_' found, do nothing.
-            } else {
-              // Check if the page is Home, we don't display it here to keep it hardcoded on the top
-              if ($docfile === 'Home.md' OR $docfile === 'README.md') {
-              // Home.md found, do nothing
-              } else {
-                // We have a valid markdown page
-                // Clean filename to get only name without extension :
-                $docfile = preg_replace('/\\.[^.\\s]{2,4}$/', '', $docfile);
-                // reset the status of active page.
-                $status="";
-                // Is there a chance we display the page of this button now? time to set status actif!
-                if ($docfile === $docpage) {
-                  $status="active";
-                }
-                // Ok, we have all, diplay this button now :
-                // Better name
-                $docfilehuman = str_replace('_', ' ', $docfile);
-                $docfilehuman = preg_replace('/[0-9.]+/', '', $docfilehuman);
-                echo '<a class="docmenuitem '.$status.'" href="';
-                $plxShow->urlRewrite('?static14/documentation&page='.$docfile);
-                echo '" title="">'.$docfilehuman.'</a>';
-              }
-            }
-          }
-        }
-        ?>
-      </section>
-      <div style="clear:both"></div>
-    </aside>
-
-    <section class="docbox col sml-12 med-8">
-    <?php 
-    # English only notification:
-      if ($lang !== 'en') {
-        echo '<div class="limit col sml-12 med-10 lrg-9 sml-centered lrg-centered med-centered sml-text-center">';
-        echo '&nbsp;<img class="svg" src="themes/peppercarrot-theme_v2/ico/nfog.svg" alt=" "/>';
-        $plxShow->lang('LIMITATIONS');
-        echo '</div>';
-      }
-    # toolbar buttons
-    echo '<div class="doctopbuttons">';
-     echo '<a href="https://framagit.org/peppercarrot/documentation/commits/master/'.$docpage.'.md" target="_blank" title="External history link to see all changes made to this page" ><img width="16px" height="16px" style="padding:0px;margin:0px" src="themes/peppercarrot-theme_v2/ico/history_b.svg" alt=""/>&nbsp;View history</a>&nbsp;&nbsp;&nbsp;&nbsp;';
-     echo '<a href="https://framagit.org/peppercarrot/documentation/edit/master/'.$docpage.'.md" target="_blank" title="Edit this page with an external editor" ><img width="16px" height="16px" style="padding:0px;margin:0px" src="themes/peppercarrot-theme_v2/ico/edit_b.svg" alt=""/>&nbsp;Edit</a>';
-    echo '</div>';
-    # Display content:
-      $contents = file_get_contents('data/documentation/'. $docpage .'.md');
-      $Parsedown = new Parsedown();
-      echo $Parsedown->text($contents);
-    ?>
-
-    <footer class="col docfoot sml-12 med-12 lrg-12 text-center">
-    <?php
-      $contents = file_get_contents('data/documentation/_footer.md');
-      $Parsedown = new Parsedown();
-      echo $Parsedown->text($contents);
-    ?>
-    </footer>
-    
-    <div style="clear:both">
-    </div>
-    
-    <br/>
-    <br/>
-
-    </section>
-    
-	</main>
-</div>
-<?php include(dirname(__FILE__).'/footer.php'); ?>
--- themes/peppercarrot-theme_v2/static-wiki.php
+++ themes/peppercarrot-theme_v2/static-wiki.php
@@ -2,126 +2,12 @@
 include(dirname(__FILE__).'/header.php');
 include(dirname(__FILE__).'/lib-parsedown.php');
 
-// Get 'page' in URL and cleanup for prohibiting code injection or path
-$page = htmlspecialchars($_GET["page"]);
-$page = preg_replace('/[^A-Za-z0-9\._-]/', '', $page);
-
 // Setup
 $repositoryURL = "https://framagit.org/peppercarrot/wiki";
 $currentpage = "?static8/wiki";
 $datapath = "data/wiki/";
 
-// Variable
-$status = "";
-$statushome = "";
+include(dirname(__FILE__).'/lib-wiki.php');
 
-// Redirection
-if(!isset($_GET['page'])) {
-  // no page found
-  $page = "README";
-  $statushome="active";
-}
-
-// Initiate page
-echo '<div class="container">'."\n";
-echo '  <main class="main grid" role="main">'."\n";
-
-// Start sidebar
-echo '    <aside class="aside col sml-12 med-3" role="complementary">'."\n";
-echo '      <section class="catbuttonmenu col sml-12 med-12 lrg-12" style="padding:0 0;">'."\n";
-echo '        <a class="catbutton '.$statushome.'" href="'; $plxShow->urlRewrite(''.$currentpage.''); echo '" title="">'."\n";
-echo '          Home'."\n";
-echo '        </a>'."\n";
-
-// Menu generation
-// Scan for markdowns
-$search = glob("".$datapath."*.md");
-if (!empty($search)){ 
-  foreach ($search as $file) {
-    // Clean full path to filename only
-    $file = basename($file);
-    
-    // Exclude _Footer, _Sidebar, and all page starting with '_'
-    if (substr($file, 0, 1) === '_') {
-    } else {
-      // Hardcode Home.md or README to the top
-      if ($file === 'Home.md' OR $file === 'README.md') {
-      } else {
-        // Make filename without extension
-        $file = preg_replace('/\\.[^.\\s]{2,4}$/', '', $file);
-        $status="";
-        // Create menu item
-        if ($file === $page) {
-          $status="active";
-        }
-        echo '        <a class="catbutton '.$status.'" href="'; $plxShow->urlRewrite(''.$currentpage.'&page='.$file); echo '" title="">'."\n";
-        echo '          '.$file.''."\n";
-        echo '        </a>'."\n";
-      }
-    }
-  }
-}
-echo '      </section>'."\n";
-echo '     <div style="clear:both"></div>'."\n";
-echo '     <br/>'."\n";
-
-// Start edit buttons
-echo '     <div class="edit" >'."\n";
-echo '      <a href="'.$repositoryURL.'/edit/master/'.$page.'.md" target="_blank" title="Edit this page with an external editor" >'."\n";
-echo '        <img width="16px" height="16px" src="themes/peppercarrot-theme_v2/ico/edit.svg" alt=""/>'."\n";
-echo '          Edit this page'."\n";
-echo '      </a>'."\n";
-echo '      <br/><br/>'."\n";
-
-echo '      <a href="'.$repositoryURL.'/commits/master/'.$page.'.md" target="_blank" title="External history link to see all changes made to this page" >'."\n";
-echo '        <img width="16px" height="16px" src="themes/peppercarrot-theme_v2/ico/history.svg" alt=""/>'."\n";
-echo '          Page history'."\n";
-echo '      </a>'."\n";
-echo '      <br/>'."\n";
-
-echo '      <a href="'.$repositoryURL.'" target="_blank" title="External repository link" >'."\n";
-echo '        <img width="16px" height="16px" src="themes/peppercarrot-theme_v2/ico/git.svg" alt=""/>'."\n";
-echo '          Repository'."\n";
-echo '      </a>'."\n";
-echo '      <br/>'."\n";
-
-echo '      <a href="'.$repositoryURL.'/commits/master" target="_blank" title="External history link to see all changes made to the Wiki" >'."\n";
-echo '        <img width="16px" height="16px" src="themes/peppercarrot-theme_v2/ico/log.svg" alt=""/>'."\n";
-echo '          Full log'."\n";
-echo '       </a>'."\n";
-
-echo '     </div>'."\n";
-echo '    </aside>'."\n";
-  
-
-echo '	  <section class="col sml-12 med-9">'."\n";
-echo '	    <div class="limit col sml-12 med-10 lrg-9 sml-centered lrg-centered med-centered sml-text-center">'."\n";
-if ($lang !== 'en') {
-  echo '&nbsp;<img class="svg" src="themes/peppercarrot-theme_v2/ico/nfog.svg" alt=" "/>';
-  $plxShow->lang('LIMITATIONS');
-}
-echo '	    </div>'."\n";
-
-// display content
-$contents = file_get_contents(''.$datapath.''. $page .'.md');
-$Parsedown = new Parsedown();
-echo $Parsedown->text($contents);
-
-echo '	    <br/><br/>'."\n";
-      
-// Footer
-echo '	    <footer class="col sml-12 med-12 lrg-12 text-center">'."\n";
-$contents = file_get_contents(''.$datapath.'_Footer.md');
-$Parsedown = new Parsedown();
-echo $Parsedown->text($contents);
-echo '	    </footer>'."\n";
-      
-echo '	    <div style="clear:both"></div>'."\n";
-echo '	    <br/><br/>'."\n";
-
-echo '	  </section>'."\n";
-echo '	</main>'."\n";
-echo '</div>'."\n";
-echo ''."\n";
 include(dirname(__FILE__).'/footer.php'); 
 ?>
