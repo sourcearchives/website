a61431be7f5bdf64ebd29e64f04744744d3860b6
David Revoy <info@davidrevoy.com>
2019-06-02 19:59:13 +0200

    Clean 'author' page from all embed style and redo the indentation.

--- themes/peppercarrot-theme_v2/static-author.php
+++ themes/peppercarrot-theme_v2/static-author.php
@@ -1,92 +1,64 @@
 <?php include(dirname(__FILE__).'/header.php'); ?>
-<?php include(dirname(__FILE__).'/lib-parsedown.php'); ?>
 <div class="container">
   <main class="main grid" role="main">
-
-		<section class="col sml-12">
+    <section class="col sml-12">
 
       <div class="grid">
-
-        <div class="col sml-12 sml-text-right">
-          <nav class="nav" role="navigation">
-            <div class="responsive-langmenu">
-              <label for="langmenu"><span class="translabutton"><img src="themes/peppercarrot-theme_v2/ico/language.svg" alt=""/> <?php echo $langlabel;?><img src="themes/peppercarrot-theme_v2/ico/dropdown.svg" alt=""/></span></label>
-              <input type="checkbox" id="langmenu">
-              <ul class="langmenu expanded">
-              <?php eval($plxShow->callHook('MyMultiLingueStaticLang')) ?>
-              <li class="button"><a class="lang" href="<?php $plxShow->urlRewrite('?static14/documentation&page=010_Translate_the_comic') ?>"><img src="themes/peppercarrot-theme_v2/ico/add.svg" alt="+"/> <?php $plxShow->lang('ADD_TRANSLATION') ?></a></li>
-              </ul>
-            </div>
-          </nav>
-        </div> 
-       
-       <article class="col sml-12 med-12 lrg-12 text-center" role="article" >
-           <figure style="margin-top: 1rem;">
-           <a href="data/images/static/2015_Portrait-of-David-Revoy_by-Elisa_de_Castro_Guerra.jpg" title="David Revoy, Photo credit : Elisa de Castro Guerra " alt="Photo of David Revoy" ><img src="data/images/static/david-revoy_photo.jpg" title="David Revoy, Photo credit : Elisa de Castro Guerra " alt="Photo of David Revoy"></a>
-           </figure>
-        <h1>
-<?php $plxShow->lang('AUTHOR_TITLE') ?>
-        </h1>
-    </div>
-    
-    <div style="max-width: 850px; margin: 0 auto; ">
-    <?php $plxShow->lang('AUTHOR_BIO') ?>
-    </div>
-
-        <div align="center" style="font-size:2em;">
-            <a href="http://www.davidrevoy.com" style="text-decoration:none;" title="www.davidrevoy.com">
-                www.davidrevoy.com
-            </a>
-        </div>
+        <?php include(dirname(__FILE__).'/lib-transla-static.php'); ?>
+        <article class="col sml-12 med-12 lrg-12" role="article" >
         
-        <div align="center" style="font-size:1.4em;">
-            <a href="mailto:info@davidrevoy.com" style="text-decoration:none;" title="Contact David Revoy by email">
-                info@davidrevoy.com
+          <figure style="margin-top: 1rem;">
+            <a href="data/images/static/2015_Portrait-of-David-Revoy_by-Elisa_de_Castro_Guerra.jpg" title="David Revoy, Photo credit : Elisa de Castro Guerra " alt="Photo of David Revoy" >
+              <img src="data/images/static/david-revoy_photo.jpg" title="David Revoy, Photo credit : Elisa de Castro Guerra " alt="Photo of David Revoy">
             </a>
-        </div>
-    <div style="max-width: 850px; margin: 0 auto; ">
-    <?php $plxShow->lang('AUTHOR_TODO_DREAM') ?>
-    </div>
-
-    <h3 align="center">
-        <?php $plxShow->lang('AUTHOR_CARREER_TITLE') ?>
-    </h3>
-
-    <p align="center">
-        <img src="data/images/static/2015-02-11_author-achivements.jpg" alt="" />
-    </p>
-    
-    <div style="max-width: 850px; margin: 0 auto; text-align:center; ">
-    <?php $plxShow->lang('AUTHOR_CARREER_BUBBLE_DESCRIPTIONS') ?>
-    </div>
-    
-
-    <p align="right" style="color:#999">
-                Photo credit : Elisa de Castro Guerra
-    </p>
-    
-    <?php
-    # Dynamic: can be edited here: 
-    # https://framagit.org/peppercarrot/webcomics/blob/master/AUTHORS.md
-    $contributorfilepath = '0_sources/AUTHORS.md';
-    $contents = file_get_contents($contributorfilepath);
-    $Parsedown = new Parsedown();
-    echo '<div style="text-align:left;">';
-    echo $Parsedown->text($contents);
-    echo '</div>';
-    ?>
-       </article> 
-
+          </figure>
           
-            <div class="col sml-12 text-center">
-                <br/><?php $plxShow->lang('TRANSLATED_BY') ?>
-            </div>
+          <em>
+          Photo credit : Elisa de Castro Guerra
+          </em>
 
+          <h1>
+            <?php $plxShow->lang('AUTHOR_TITLE') ?>
+          </h1>
+          
+          <?php $plxShow->lang('AUTHOR_BIO') ?>
 
-		</section>
-    
+          <div class="button big moka">
+            <a href="http://www.davidrevoy.com" style="text-decoration:none;" title="www.davidrevoy.com">
+              www.davidrevoy.com
+            </a>
+          </div>
+          
+          <br/>
 
+          <div class="button moka">
+            <a href="mailto:info@davidrevoy.com" style="text-decoration:none;" title="Contact David Revoy by email">
+              info@davidrevoy.com
+            </a>
+          </div>
 
-	</main>
+          <?php $plxShow->lang('AUTHOR_TODO_DREAM') ?>
+
+          <h3>
+            <?php $plxShow->lang('AUTHOR_CARREER_TITLE') ?>
+          </h3>
+
+          <img src="data/images/static/2015-02-11_author-achivements.jpg" alt="" />
+
+          <?php $plxShow->lang('AUTHOR_CARREER_BUBBLE_DESCRIPTIONS') ?>
+
+          <?php
+          # Listing here: https://framagit.org/peppercarrot/webcomics/blob/master/AUTHORS.md
+          include(dirname(__FILE__).'/lib-parsedown.php');
+          $contributorfilepath = '0_sources/AUTHORS.md';
+          $contents = file_get_contents($contributorfilepath);
+          $Parsedown = new Parsedown();
+          echo $Parsedown->text($contents);
+          ?>
+        </article>
+      </div>
+      
+    </section>
+  </main>
 </div>
 <?php include(dirname(__FILE__).'/footer.php'); ?>
