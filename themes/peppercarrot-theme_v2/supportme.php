87551c6431fe3b79b9c6662ae57b21d090757c0d
David Revoy <info@davidrevoy.com>
2019-06-03 10:37:59 +0200

    Remove support-me php file
    It was used on the footer of the webcomic during a long time; but
    now the header top bar is always visible with a donate button; it
    is not necessary.

--- themes/peppercarrot-theme_v2/supportme.php
+++
@@ -1,9 +0,0 @@
-<?php if(!defined('PLX_ROOT')) exit;?>
-
-    <div style="max-width: 640px; margin: 0 auto;"><?php $plxShow->lang('DONATE_INTRO') ?></div>
-    <?php $plxShow->lang('HOMEPAGE_ALTERNATIVES') ?>
-    <a href="https://paypal.me/davidrevoy" title="Send money via Paypal">Paypal</a>, 
-    <a href="https://www.tipeee.com/pepper-carrot" title="<?php $plxShow->lang('PATRONAGE_BUTTON') ?> Tipeee">Tipeee</a>,
-    <a href="https://liberapay.com/davidrevoy/" title="<?php $plxShow->lang('PATRONAGE_BUTTON') ?> Liberapay">Liberapay</a>,
-    <a href="https://g1.duniter.fr/#/app/wot/4nosBEwT8xQfMY11sq32AnZF1XcoqzG9tArXJq9mu8Wc/DavidRevoy" title="Send G1 to David Revoy">G1</a>,
-    <a href="<?php $plxShow->urlRewrite('?static12/iban-and-mail-adress') ?>" title="Send money via IBAN or Send goods via mail">Bank/Mail</a>.
