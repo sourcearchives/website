a9c7a1ce84823895af8ad4ac7deacde17595ca3f
David Revoy <info@davidrevoy.com>
2019-06-05 11:59:23 +0200

    Theme the prev/next top header buttons
    They need to be subtle, not in the way.
    I also shortened the French label to be more effective.

--- themes/peppercarrot-theme_v2/lang/fr.php
+++ themes/peppercarrot-theme_v2/lang/fr.php
@@ -255,8 +255,8 @@ $LANG = array(
 
 ################################################################################
 # various utils :
-'UTIL_NEXT_EPISODE'     =>  'épisode suivant',
-'UTIL_PREVIOUS_EPISODE' =>  'épisode précédent',
+'UTIL_NEXT_EPISODE'     =>  'suivant',
+'UTIL_PREVIOUS_EPISODE' =>  'précédent',
 'UTIL_EPISODE'   =>  'épisode',
 'UTIL_BACK_TO_GALLERY'      =>  'retour à la galerie',
 'UTIL_MORE'      =>  'plus',
