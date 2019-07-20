ff1256233bc55ac590f27ac74205bee3960fbacc
Gleki Arxokuna <gleki.is.my.name@gmail.com>
2019-07-20 16:09:46 +0200

    Update jb.php

--- themes/peppercarrot-theme_v2/lang/jb.php
+++ themes/peppercarrot-theme_v2/lang/jb.php
@@ -40,7 +40,7 @@ $LANG = array(
     i e\'e jau gau cenba fai lo gundi be lo lisri<br/>
 ',
 'HOMEPAGE_PATREON_INFO'     =>  'e\'u do\'e ro pagbu be lo lisri do ta\'i lo ka dunda lo merko rupnu be li pa co\'a selfu do\'e la\o .gy.Patreon.gy.',
-'HOMEPAGE_MOREINFO_BUTTON'  =>  'drata datni',
+'HOMEPAGE_MOREINFO_BUTTON'  =>  'tcila',
 'HOMEPAGE_PATREON_BUTTON'   =>  "co'a sidju la'e la piper joi la karot sepi'o la'o gy.Patreon.gy.",
 'PATRONAGE_BUTTON'          => "co'a sidju la'e la piper joi la karot",
 'HOMEPAGE_LAST_EPISODE'     =>  'ro moi pagbu',
