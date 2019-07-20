61aafbca92775a3f7a1b6bdde90b769a145e9361
Gleki Arxokuna <gleki.is.my.name@gmail.com>
2019-07-20 15:30:58 +0200

    Add new file

---
+++ themes/peppercarrot-theme_v2/lang/jb.php
@@ -0,0 +1,255 @@
+<?php
+
+$LANG = array(
+
+# Global
+'TRANSLATED_BY' 		    => 'fanva fa <a href="https://la-lojban.github.io/sutysisku/">la gleki arxokuna</a>',
+'LANGUAGE_NAME' 			=> 'Lojban',
+'LANGUAGE_ISO_CODE_2_LETTER'=> 'jb',
+# http://www.w3schools.com/tags/ref_language_codes.asp --> full list
+
+################################################################################
+# Header HTML infos for search engine and title in tab:
+'PEPPERCARROT_VEGETABLE'    =>  'la piper jo\'u la karot', // litteral translation of spices + vegetable
+'Website_DESCRIPTION'        =>  "lo noi se ke catni zanru vau stuzi la\'e lu la piper jo\'u la karot li\'u to i la\'e bu se jdima be no da gunma lo lisri be la piper e la karot i je la piper cu citno te makfa i je la karot mlatu gi'e se raltu la piper i la piper e la karot cu xabju lo se cfika munje be lo mixre e lo danlu vu'o poi makfu",
+'SUBTITLE'                  =>  'se jdima be no da gunma lo pixra gi\'e se finti la\'o gy.David Revoy.gy.',
+
+################################################################################
+# Top menu website:
+'HOMEPAGE'      =>  'zdani',
+'WEBCOMICS'     =>  'pixra lisri',
+'BLOG'          =>  'karni',
+'PHILOSOPHY'    =>  'tadji',
+'CONTRIBUTE'    =>  'e\'a do finti',
+'SOURCES'       =>  'ka ve zbasu',
+'AUTHOR'        =>  'finti',
+'FOLLOW'        =>  'zgana lo nuzba',
+
+################################################################################
+# Top and bottom translation panel
+'ADD_TRANSLATION'       => 'jmina lo te traduki',
+'CORRECTIONS' 			=> 'stidi lo ni cikre',
+
+################################################################################
+# Page: Homepage
+'HOMEPAGE_BIG_TEXT'		    =>  '
+    i me lo lisri poi se jdima be no da<br/>zi\'e poi lo cupra cartu be ke\'a cu gubni</br>
+    zi\'e noi se sarji lo se selfu<br/>
+    i e\'e jau gau cenba fai lo gundi be lo lisri<br/>
+',
+'HOMEPAGE_PATREON_INFO'     =>  'e\'u do\'e ro pagbu be lo lisri do ta\'i lo ka dunda lo merko rupnu be li pa co\'a selfu do\'e la\o .gy.Patreon.gy.',
+'HOMEPAGE_MOREINFO_BUTTON'  =>  'drata datni',
+'HOMEPAGE_PATREON_BUTTON'   =>  'co\'a selfu la\'e la piper jo\'u la karot sepi\'o la\'o gy.Patreon.gy.',
+'HOMEPAGE_LAST_EPISODE'     =>  'ro moi pagbu',
+'HOMEPAGE_NEWS_UPDATE'      =>  'nuzba i cnino',
+'HOMEPAGE_MOREPOSTS_BUTTON' =>  'drata se benji',
+'HOMEPAGE_MAINSERVICE_LINK' =>  'https://www.patreon.com/davidrevoy',
+
+################################################################################
+# Page: Webcomics
+'WEBCOMIC_EPISODE'		    =>  'pagbu lo lisri',
+'WEBCOMIC_MAKINGOF'		    =>  'tadji lo nu zbasu',
+'WEBCOMIC_MAKINGOF_DESCRIPTION' =>  '
+    <p> i lo <a href="https://www.patreon.com/davidrevoy">nobli se selfu pe mi</a> cu dunda lo jdini i mi pilno ri lo ka zbasu <a href="http://www.davidrevoy.com/categorie5/tutorials">lo ctuca cukta</a> e lo vreji be lo tadji be lo nu zbasu i le cukta cu se ke steci dunda fi\'i lo pu\'o pilno</p>
+',
+'WEBCOMIC_ARTWORK'		    =>  'muzga lo pixra',
+'WEBCOMIC_SKETCHES'		    =>  'tsautu',
+
+################################################################################
+# Page: Blog
+# when content are not English (no need to warn user the content is english only here):
+#'LIMITATIONS' 	=> '<i class="fa fa-info-circle"></i> Content available in english only ',
+'LIMITATIONS' 		=> '',
+
+################################################################################
+# Page: Philosophy
+'PHILOSOPHY_CONTENT'    =>  '
+ <h2>i se sarji lo se selfu</h2>
+
+    <p>i la\'e la piper jo\'u la karot cu se sraji lo noi xabju lo vrici gugde vau se selfu be la\'e bu e no drata<br/>
+    i ro selfu cu dunda lo so\'o jdini do\'e ro cnino pagbu be lo lisri i je lo romoi papri be le pagbu cu uencu lo liste be le selfu<br/>
+    i la\'e di\'u rinka lo nu la\'e la piper jo\'u la karot cu se jdima li no gi\'e na srana lo notci be lo nu vecnu</p>
+
+    <div class="philobutton">
+        <a href="https://www.patreon.com/davidrevoy" title="e\'u do\'e ro pagbu be lo lisri do ta\'i lo ka dunda lo merko rupnu be li pa co\'a selfu do\'e la\o .gy.Patreon.gy.">
+            e\'u do\'e ro pagbu be lo lisri do ta\'i lo ka dunda lo merko rupnu be li pa co\'a selfu do\'e la\o .gy.Patreon.gy.
+        </a>
+    </div>
+
+        <a href="https://www.patreon.com/davidrevoy" title="e\'u do\'e ro pagbu be lo lisri do ta\'i lo ka dunda lo merko rupnu be so\'u li pa co\'a selfu do\'e la\o .gy.Patreon.gy.">
+            <img alt="nu selfu" src="data/images/static/2015-02-09_philosophy_01-support.jpg">
+        </a>
+
+    <h2>i je\'u ze\'e jdima be no da</h2>
+
+    <p>i ro da poi se finti mi gi\'e skicu la piper jo\'u la karot gi\'e se stuzi lo dei kibro cu se jdima no de i je ro prenu jai se krali fai lo ka se pilno le se finti<br/>
+    i lo dunda be lo jdini cu dunli lo na dunda be lo jdini lo ka se sinma mi i mi cikre lo dunda be lo jdini ta\'i lo ka dunda le dunda lo selte\'i<br/>
+    i lo prenu ba no roi jai sarcu fai lo ka dunda lo jdini vau fe lo nu tcidu lo cnino pagbu be lo lisri be la piper jo\'u la karot</p>
+
+        <img alt="lo se dunda be fi lo selfu" src="data/images/static/2015-02-09_philosophy_03-paywall.jpg">
+
+    <h2>i lo cupra cartu cu gubni i je lo ka pilno ri cu krali ro da</h2>
+
+    <p>i mi curmi lo nu do zbasu lo fukpi be lo se finti be mi gi\'a pilno lo mi\'u se finti gi\'e ji\'a jerna fi lo ka pilno mi\'u<br/>
+    i ro da poi papri gi\'a pixra gi\'a cfika zi\'e poi srana zo\'u mi pilno fi lo ka zbasu da vau fe lo proga poi se jdima no de zi\'e poi lo cupra cartu be ke\'a cu gubni zi\'e noi mi pilno va\'o tu\'a la\'o gy.GNU/Linux.gy. i ro cupra vreji ka\'e se cpacu fi lo se cuxna poi du la cupra<br/>
+i mi e\'ende do lo ka va\'o la\'e di\'e jerna pilno gi\'a fanva gi\'a finti lo poi srana la\'e la piper jo\'u la karot vau pixra gi\'a zbasu lo prina gi\'a finti lo skina a lo se kelci gi\'e jau gau djuno fai lo drata fi la piper jo\'u la karot<br/>
+i sa\'u do jmina lo notci be ro da poi finti gi\'a fanva gi\'a cikre vau lo se pilno be do i je do jmina lo judri be lo javni be lo nu pilno i je ga nai do cnegau de gi do jmina lo notci be lo nu mi\'u cnegau i je do na jmina lo notci be lo nu lo finti cu zanru lo se gunka be do a lo nu do pilno mi\'u</p>
+    <div class="philobutton">
+        <a href="https://creativecommons.org/licenses/by/4.0/" title="i do ka\'e cilre lo zmadu ta\'i lo ka tcidu lo srana be la\o gy.Creative Commons Attribution 4.0.gy.">
+            i jaspu fa la\'o gy.License: Creative Commons Attribution 4.0.gy.
+        </a>
+    </div>
+
+        <img alt="pixra fingubni se uencu" src="data/images/static/2015-02-09_philosophy_04-open-source.jpg">
+
+    <h2>i ai zabna nu zdile fa ro do bu\'u ro da</h2>
+
+    <p>la\'e la piper jo\'u la karot cu gunma lo xajmi lisri suited for everyone, every age.<br/>
+    i no vreji be lo nu gletu a lo nu vlile cu se curmi  .i la\'e la piper jo\'u la karot lu\'u noi se jdima no da gi\'e fingubni cu jai se jgira mupli lo ka zabna vau lo jai se zifre jai kulnu<br/>
+    .i mi zukte fi lo nu lo se finti cu carmi zabna i ki\'u bo so\'e prenu cu jinvi lo du\'u so\'a fingubni cu mabla gi\'a jai se to\'e certu .i .ai darlu fi du\'u bu</p>
+
+        <img alt="" src="data/images/static/2015-02-09_philosophy_05-everyone.jpg">
+
+    <h2>i e\'e gau mi\'o lo gundi be lo se pixra lisri cu cenba</h2>
+
+    <p>Without intermediary between artist and audience you pay less and I benefit more. You support me directly.<br/>
+    No publisher/distributor/marketing/fashion can force me to change Pepper&amp;Carrot to fit their vision of &#39;the market&#39;.<br/>
+    &nbsp;.&nbsp;.&nbsp;.&nbsp;Why not a single success couldn&#39;t &#39;snowball&#39; a whole industry in crisis? We&#39;ll see...</p>
+
+        <img alt="i lo gundi be lo nu zbasu lo cukta be lo pixra i je ku\'i sidju fi lo ka selfu" src="data/images/static/2015-02-09_philosophy_06-industry-change.jpg">
+
+    <div class="philobutton">
+        <a class="h4" href="https://www.patreon.com/davidrevoy" title="e\'u do\'e ro pagbu be lo lisri do ta\'i lo ka dunda lo merko rupnu be li so\'u pa do catni se selfu sidju mi lo ka finti lo cnino pagbu be lo lisri be la piper jo\'u la karot
+        </a>
+    </div>
+
+    <p>i mi ckire do lo ka tcidu <br/>
+    mi\'e la\'o gy.David Revoy.gy.</p>
+
+ ',
+
+################################################################################
+# Page: Contribute
+'CONTRIBUTE_TITLE'		=> 'sidju',
+'CONTRIBUTE_TOP'		=> '
+    <p>la'e la piper jo'u la karot cu <a href="?static6/sources" title="Sources page">fingubni</a> gi'e se jaspu jaspu lo Thanks to the  and the .i la'o gy.<a href="http://creativecommons.org/licenses/by/4.0/">
+    creative commons license</a>.gy. jaspu fo lo nu do ka'e finti so'i srana be la piper e la karot</p>
+',
+'CONTRIBUTE_DONATION'		=> '
+    <h2>i lo nu selfu i lo nu dunda</h2>
+
+    <p>I detail in the <a href="?static2/philosophy" title="open the Sources page">Philosophy page</a> almost everything concerning funding.<br/>
+    .i sei frili lo ka co'a selfu la'e la pipep jo'u la karot cu se tadji lo ka dunda tu'a lo pa rupnusudu <a href="https://www.patreon.com/davidrevoy">do'e la'o gy.Patreon.gy.</a> ba ro nu co'i finti lo cnino pagbu be lo lisri be py. jo'u cy.<br/>
+    Patreon accept credit cards from all around the world, and you can even pay with a Paypal account on Patreon.<br/>
+            I also accept direct donations via PayPal or Flattr:</p>
+',
+'CONTRIBUTE_FANART'	    => '
+    <h2>i lo se finti be lo nelci</h2>
+    <p>i ka\'e finti lo arte pe la\'e la piper jo\'u la karot: Drawing, scenarios, scupltures, 3D models, fan-fiction. Send them to me (<a href="mailto:info@davidrevoy.com">info@davidrevoy.com</a>, or poke me on social networks) to appear in the fan-art gallery:</p>
+',
+'CONTRIBUTE_DERIVATIONS' => '
+    <h2>lo se jicmu be lo se finti be mi</h2>
+    <p>Pepper&amp;Carrot can be adapted to many project or products, why not do your own or join an existing one?</p>
+',
+
+'CONTRIBUTE_BOTTOM'	    => '
+    <h2>i lo te traduki e lo nu cikre</h2>
+
+    <p>Pepper&amp;Carrot website is designed to be multilingual and accept any language (including extinct ones, or fictive ones). The sources of this page are at your disposal for you to translate them. Check the <a href="index.php?fr/article267/translation-tutorial">reference tutorial</a> for more information on how to add your translation.</p>
+
+    <h2>.i karni .i tcana lo nu jikca</h2>
+
+    <p>Be the publisher of Pepper&amp;Carrot! Write articles, create posts on websites, share and build a community on your favorite social networks. You can download the <a href="?static6/sources" title="Sources page">press kit</a> on the "sources" page.</p>
+
+    <h2>Other contributions ideas&nbsp;.&nbsp;.&nbsp;.&nbsp;</h2>
+
+    <p>i ro prenu ka\'e sidju<br/>
+        <b>i lo jai gau farvi</b> cu go\'i fi lo ka proga be lo nu tcidu do\'e la piper jo\'u la karot bei do\'e lo cmalu skami<br/>
+        <b>i lo finti be lo zgike</b> cu ka\'e finti lo zgike poi srana py. jo\'u ky.<br/>
+        <b>i lo finti be lo prosa</b> ka\'e stidi tu\'a lo cnino lisri be py. jo\'u cy.<br/>
+        <b>Journalist:</b> Inform about Pepper&amp;Carrot in traditionnal media (printed press, tv, etc.)<br/>
+        <b>Printer:</b> Print posters or goodies with Pepper&amp;Carrot on it.<br/>
+    </p>
+
+    <h2>i di\'e se irci</h2>
+
+    <p>.i e\'e do irci gi\'e casnu la piper jo\'u la karot .i mi ta\'e jundi ca lo ropno donri i zoi zoi.deevad.zoi. cmene mi lo se irci i mi tavla bau lo fraos e lo glico<br/>
+',
+
+################################################################################
+# Page: Sources
+'SOURCES_TITLE'		    => 'vreji lo ve zbasu',
+'SOURCES_TOP'	        => '
+    <p><b>Welcome to the sources download center!</b><br/>
+    You&#39;ll find here the sources files of Pepper&amp;Carrot and more.<br/>
+    All digital-painting files here are compatible with the latest version of <a href="https://krita.org/">Krita</a>.</p>
+',
+'SOURCES_BOTTOM'	    => '
+    <p>By downloading and working with those files you agree to respect the terms of the <br/>
+    <a href="http://creativecommons.org/licenses/by/4.0/">Creative Commons Attribution license</a>.
+    Check the README files in each project for more infos.</p>
+
+    <p><b><i class="fa fa-github" style="font-size:1.5em;" ></i> Code repositories for translations, script, website, graphism,etc.:</b><br/></p>
+',
+'SOURCE_COVER'   =>  'gacri pixra',
+'SOURCE_KRITA'   =>  'lo cupra vreji pe la krita',
+'SOURCE_TRANSLATOR'=>  'vreji be do\'e lo traduki',
+'SOURCE_WEB'      =>  'i uebre i na\'e barda me JPG vreji i <span class="sourceinfos">bu\'u ro inci 90 pikseli</span>',
+'SOURCE_PRINT'        =>  'i prina i no pikseli cu cenba i <span class="sourceinfos">bu\'u ro inci 300 pikseli</span>',
+'SOURCE_MONTAGE'   =>  'i muzga <span class="sourceinfos">i pa uencu lo ro papri</span>',
+
+################################################################################
+# Page: Author
+'AUTHOR_TITLE'		=> 'srana la\'o gy.David Revoy.gy.',
+'AUTHOR_BIO'	    => '
+    <p>coi do mi se cmene zoi gy.David Revoy.gy. gi\'e fraso finti be lo pixra gi\'e de\'i li nanca bu 1981 co\'a. .i mi ctuca mi gi'e nelci lo nu zbasu lo pixra vau e lo mlatu e lo skami e lo kulnu be lo pilno be la'o gy.Gnu/Linux.gy. noi lo jicmu be ke'a cu fingubni i ji'a mi nelci lo kibro e lo RPG laldo se kelci e lo laldo pixra je lisri e lo tcaci arte e lo kulnu be lo ponjo be'o e lo fantasi zei cfika li'o<br/>
+    <br/>
+    After more than 10 years of Freelance in digital-painting, teaching, concept-art, illustration, art-direction, I decided to start my own project. I finally found a way to mix all my passions together; and the result is Pepper&amp;Carrot.</p>
+
+    <p>.i lo jai se gunka be mi</p>
+',
+'AUTHOR_TODO_DREAM'	=> '
+    <h3>i mi djica tu\'a la\'e di\'e</h3>
+
+    <p>
+    ☐ i tavla fi la piper e la karot au lo ka vitke fi lo te penmi be lo ponjo finti be lo pixra lisri<br/>
+    ☐ i da se skami se kelci  do\'e la piper jo\'u la karot<br/>
+    ☐ i gasnu lo nu su\'o 100 pixra poi se finti lo drata co\'a zasti<br/>
+    ☑ <a href="http://www.peppercarrot.com/en/article376/dream-to-do-list-a-wikipedia-page">i da papri do\'e la uikipediias</a><br/>
+    ☐ i cpacu lo se kacma pixra be lo dasni bei tai tu\'a la piper<br/>
+    ☑ <a href="http://www.peppercarrot.com/article304/dream-to-do-list-a-cat-named-carrot">i cpacu lo se kacma pixra be lo pa narju mlatu poi se cmene zo karot</a><br/>
+    ☑ <a href="http://www.peppercarrot.com/article302/peppercarrot-has-over-500-supporters">i selfu su\'o 500 da</a><br/>
+    ☐ i lo pagbu cu su\'o 100 mei<br/>
+    </p>
+',
+
+'AUTHOR_CARREER_TITLE'				 => 'i citri be mi gi\'e pixra 7 mei',
+'AUTHOR_CARREER_BUBBLE_DESCRIPTIONS' => '<p></p>',
+
+################################################################################
+# Page: Wiki
+'WIKI'		=> 'uitki',
+
+################################################################################
+# Website General: Footer
+'FOOTER_CONTENT'    => '
+    <p>le gunma be le pixra be\'o e ro le pixra e lo srana uenzi cu se jaspu la\'o zoi.<a href="https://creativecommons.org/licenses/by/4.0/" title="ko tcidu la\'o zoi.the Creative Commons Attribution 4.0.zoi.">Creative Commons Attribution 4.0</a>.zoi. i jo nai lo mirgumi be lo papri cu xusra lo du\'u lo du\'u go\'i cu jitfa<br/>
+    i sitna la\'o fy.David Revoy.fy. e la\o zoi.www.davidrevoy.com.zoi. i ko mrilu fi la\'o zoi. <a href="mailto:info@davidrevoy.com">info@davidrevoy.com</a>.zoi. noi judri mi vau fe lo vreji be lo nu do kucli tu\'a lo drata datni.</p>
+    <p>i lo dei stuzi cu se cupra la\'o zoi.<a href="http://www.pluxml.org" title="PlXml">PluXml</a>.zoi.</p>
+',
+
+################################################################################
+# various utils:
+'UTIL_NEXT_EPISODE'      =>  'se lidne pagbu',
+'UTIL_PREVIOUS_EPISODE'  =>  'lidne pagbu',
+'UTIL_EPISODE'   =>  'pagbu',
+'UTIL_BACK_TO_GALLERY'      =>  'muzga lo pixra',
+'UTIL_MORE'      =>  'drata',
+'UTIL_PAGE'      =>  'papri',
+'UTIL_BY'        =>  '',
+'UTIL_LICENSE'   =>  'jaspu',
+'FIRST'        =>  '1 moi',
+'LAST'   =>  'ro moi',
+);
+
+?>
