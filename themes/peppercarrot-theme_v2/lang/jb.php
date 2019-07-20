5b73fab682863b1dbf1c5a557e29945a314fed46
Gleki Arxokuna <gleki.is.my.name@gmail.com>
2019-07-20 16:18:31 +0200

    Update jb.php

--- themes/peppercarrot-theme_v2/lang/jb.php
+++ themes/peppercarrot-theme_v2/lang/jb.php
@@ -10,9 +10,9 @@ $LANG = array(
 
 ################################################################################
 # Header HTML infos for search engine and title in tab:
-'PEPPERCARROT_TITLE'    =>  "la .piper. joi la .karot.", // the title on top of the website
-'PEPPERCARROT_VEGETABLE'    =>  "la .piper. joi la .karot.", // litteral translation of spices + vegetable
-'Website_DESCRIPTION'        =>  "lo noi se ke catni zanru vau stuzi la\'e lu la piper jo\'u la karot li\'u to i la\'e bu se jdima be no da gunma lo lisri be la piper e la karot i je la piper cu citno te makfa i je la karot mlatu gi'e se raltu la piper i la piper e la karot cu xabju lo se cfika munje be lo mixre e lo danlu vu'o poi makfu",
+'PEPPERCARROT_TITLE'    =>  "la piper joi la karot", // the title on top of the website
+'PEPPERCARROT_VEGETABLE'    =>  "la piper joi la karot", // litteral translation of spices + vegetable
+'Website_DESCRIPTION'        =>  "lo noi se ke catni zanru vau stuzi la\'e lu la piper joi la karot li\'u to i la\'e bu se jdima be no da gunma lo lisri be la piper e la karot i je la piper cu citno te makfa i je la karot mlatu gi'e se raltu la piper i la piper e la karot cu xabju lo se cfika munje be lo mixre e lo danlu vu'o poi makfu",
 'SUBTITLE'                  =>  'se jdima be no da gunma lo pixra gi\'e se finti la\'o gy.David Revoy.gy.',
 
 ################################################################################
@@ -62,16 +62,16 @@ $LANG = array(
 # Page: Blog
 # when content are not English (no need to warn user the content is english only here):
 #'LIMITATIONS' 	=> '<i class="fa fa-info-circle"></i> Content available in english only ',
-'LIMITATIONS' 		=> "<i class='fa fa-info-circle'></i> .i di'e nu'o se fanva fi le lojbo",
+'LIMITATIONS' 		=> "<i class='fa fa-info-circle'></i> i di'e nu'o se fanva fi le lojbo",
 
 ################################################################################
 # Page: Philosophy
 'PHILOSOPHY_CONTENT'    =>  "
  <h2>i se sarji lo se selfu</h2>
 
-    <p>i la'e la piper jo'u la karot cu se sraji lo noi xabju lo vrici gugde vau se selfu be la'e bu e no drata<br/>
+    <p>i la'e la piper joi la karot cu se sraji lo noi xabju lo vrici gugde vau se selfu be la'e bu e no drata<br/>
     i ro selfu cu dunda lo so'o jdini do'e ro cnino pagbu be lo lisri i je lo romoi papri be le pagbu cu uencu lo liste be le selfu<br/>
-    i la'e di'u rinka lo nu la'e la piper jo'u la karot cu se jdima li no gi'e na srana lo notci be lo nu vecnu</p>
+    i la'e di'u rinka lo nu la'e la piper joi la karot cu se jdima li no gi'e na srana lo notci be lo nu vecnu</p>
 
     <div class='philobutton'>
         <a href='https://www.patreon.com/davidrevoy' title='e'u do'e ro pagbu be lo lisri do ta'i lo ka dunda lo merko rupnu be li pa co'a selfu do'e la\o .gy.Patreon.gy.'>
@@ -85,9 +85,9 @@ $LANG = array(
 
     <h2>i je'u ze'e jdima be no da</h2>
 
-    <p>i ro da poi se finti mi gi'e skicu la piper jo'u la karot gi'e se stuzi lo dei kibro cu se jdima no de i je ro prenu jai se krali fai lo ka se pilno le se finti<br/>
+    <p>i ro da poi se finti mi gi'e skicu la piper joi la karot gi'e se stuzi lo dei kibro cu se jdima no de i je ro prenu jai se krali fai lo ka se pilno le se finti<br/>
     i lo dunda be lo jdini cu dunli lo na dunda be lo jdini lo ka se sinma mi i mi cikre lo dunda be lo jdini ta'i lo ka dunda le dunda lo selte'i<br/>
-    i lo prenu ba no roi jai sarcu fai lo ka dunda lo jdini vau fe lo nu tcidu lo cnino pagbu be lo lisri be la piper jo'u la karot</p>
+    i lo prenu ba no roi jai sarcu fai lo ka dunda lo jdini vau fe lo nu tcidu lo cnino pagbu be lo lisri be la piper joi la karot</p>
 
         <img alt='lo se dunda be fi lo selfu' src='data/images/static/2015-02-09_philosophy_03-paywall.jpg'>
 
@@ -95,7 +95,7 @@ $LANG = array(
 
     <p>i mi curmi lo nu do zbasu lo fukpi be lo se finti be mi gi'a pilno lo mi'u se finti gi'e ji'a jerna fi lo ka pilno mi'u<br/>
     i ro da poi papri gi'a pixra gi'a cfika zi'e poi srana zo'u mi pilno fi lo ka zbasu da vau fe lo proga poi se jdima no de zi'e poi lo cupra cartu be ke'a cu gubni zi'e noi mi pilno va'o tu'a la'o gy.GNU/Linux.gy. i ro cupra vreji ka'e se cpacu fi lo se cuxna poi du la cupra<br/>
-i mi e'ende do lo ka va'o la'e di'e jerna pilno gi'a fanva gi'a finti lo poi srana la'e la piper jo'u la karot vau pixra gi'a zbasu lo prina gi'a finti lo skina a lo se kelci gi'e jau gau djuno fai lo drata fi la piper jo'u la karot<br/>
+i mi e'ende do lo ka va'o la'e di'e jerna pilno gi'a fanva gi'a finti lo poi srana la'e la piper joi la karot vau pixra gi'a zbasu lo prina gi'a finti lo skina a lo se kelci gi'e jau gau djuno fai lo drata fi la piper joi la karot<br/>
 i sa'u do jmina lo notci be ro da poi finti gi'a fanva gi'a cikre vau lo se pilno be do i je do jmina lo judri be lo javni be lo nu pilno i je ga nai do cnegau de gi do jmina lo notci be lo nu mi'u cnegau i je do na jmina lo notci be lo nu lo finti cu zanru lo se gunka be do a lo nu do pilno mi'u</p>
     <div class='philobutton'>
         <a href='https://creativecommons.org/licenses/by/4.0/' title='i do ka'e cilre lo zmadu ta'i lo ka tcidu lo srana be la\o gy.Creative Commons Attribution 4.0.gy.'>
@@ -107,22 +107,22 @@ i sa'u do jmina lo notci be ro da poi finti gi'a fanva gi'a cikre vau lo se piln
 
     <h2>i ai zabna nu zdile fa ro do bu'u ro da</h2>
 
-    <p>la'e la piper jo'u la karot cu gunma lo xajmi lisri suited for everyone, every age.<br/>
-    i no vreji be lo nu gletu a lo nu vlile cu se curmi  .i la'e la piper jo'u la karot lu'u noi se jdima no da gi'e fingubni cu jai se jgira mupli lo ka zabna vau lo jai se zifre jai kulnu<br/>
-    .i mi zukte fi lo nu lo se finti cu carmi zabna i ki'u bo so'e prenu cu jinvi lo du'u so'a fingubni cu mabla gi'a jai se to'e certu .i .ai darlu fi du'u bu</p>
+    <p>la'e la piper joi la karot cu gunma lo xajmi lisri suited for everyone, every age.<br/>
+    i no vreji be lo nu gletu a lo nu vlile cu se curmi  i la'e la piper joi la karot lu'u noi se jdima no da gi'e fingubni cu jai se jgira mupli lo ka zabna vau lo jai se zifre jai kulnu<br/>
+    i mi zukte fi lo nu lo se finti cu carmi zabna i ki'u bo so'e prenu cu jinvi lo du'u so'a fingubni cu mabla gi'a jai se to'e certu i .ai darlu fi du'u bu</p>
 
         <img alt='' src='data/images/static/2015-02-09_philosophy_05-everyone.jpg'>
 
     <h2>i e'e gau mi'o lo gundi be lo se pixra lisri cu cenba</h2>
 
-    <p>i ri'a le nu no drata kagni cu sidju vau ge do pleji le mleca be le'e jai se kanpe gi mi cpacu le zmadu be le'e jai se kanpe .i do mi sidju co se sidju no na'e jai sarcu<br/>
+    <p>i ri'a le nu no drata kagni cu sidju vau ge do pleji le mleca be le'e jai se kanpe gi mi cpacu le zmadu be le'e jai se kanpe i do mi sidju co se sidju no na'e jai sarcu<br/>
     No publisher/distributor/marketing/fashion can force me to change Pepper&amp;Carrot to fit their vision of &#39;the market&#39;.<br/>
     &nbsp;.&nbsp;.&nbsp;.&nbsp;Why not a single success couldn&#39;t &#39;snowball&#39; a whole industry in crisis? We&#39;ll see...</p>
 
         <img alt='i lo gundi be lo nu zbasu lo cukta be lo pixra i je ku'i sidju fi lo ka selfu' src='data/images/static/2015-02-09_philosophy_06-industry-change.jpg'>
 
     <div class='philobutton'>
-        <a class='h4' href='https://www.patreon.com/davidrevoy' title='e'u do'e ro pagbu be lo lisri do ta'i lo ka dunda lo merko rupnu be li so'u pa do catni se selfu sidju mi lo ka finti lo cnino pagbu be lo lisri be la piper jo'u la karot
+        <a class='h4' href='https://www.patreon.com/davidrevoy' title='e'u do'e ro pagbu be lo lisri do ta'i lo ka dunda lo merko rupnu be li so'u pa do catni se selfu sidju mi lo ka finti lo cnino pagbu be lo lisri be la piper joi la karot
         </a>
     </div>
 
@@ -135,20 +135,20 @@ i sa'u do jmina lo notci be ro da poi finti gi'a fanva gi'a cikre vau lo se piln
 # Page: Contribute
 'CONTRIBUTE_TITLE'		=> 'sidju',
 'CONTRIBUTE_TOP'		=> "
-    <p>la'e la .piper. jo'u la .karot. cu <a href='?static6/sources' title='Sources page'>fingubni</a> gi'e se jaspu jaspu lo Thanks to the  and the .i la'o gy.<a href='http://creativecommons.org/licenses/by/4.0/'>
+    <p>la'e la piper joi la karot cu <a href='?static6/sources' title='Sources page'>fingubni</a> gi'e se jaspu jaspu lo Thanks to the  and the i la'o gy.<a href='http://creativecommons.org/licenses/by/4.0/'>
     creative commons license</a>.gy. jaspu fo lo nu do ka'e finti so'i srana be la piper e la karot</p>
 ",
 'CONTRIBUTE_DONATION'		=> "
-    <h2>.i nu selfu .i nu dunda</h2>
+    <h2>i nu selfu i nu dunda</h2>
 
     <p>mi ciska fi le uencu be <a href='?static2/philosophy' title='open the Sources page'>le tadji</a> fe ji'i ro le tcila be le nu pleji fo le te jerna<br/>
-    .i sei frili lo ka co'a selfu la'e la pipep jo'u la karot cu se tadji lo ka dunda tu'a lo pa rupnusudu <a href='https://www.patreon.com/davidrevoy'>do'e la'o gy.Patreon.gy.</a> ba ro nu co'i finti lo cnino pagbu be lo lisri be py. jo'u cy.<br/>
+    i sei frili lo ka co'a selfu la'e la pipep joi la karot cu se tadji lo ka dunda tu'a lo pa rupnusudu <a href='https://www.patreon.com/davidrevoy'>do'e la'o gy.Patreon.gy.</a> ba ro nu co'i finti lo cnino pagbu be lo lisri be py. joi cy.<br/>
     Patreon accept credit cards from all around the world, and you can even pay with a Paypal account on Patreon.<br/>
             I also accept direct donations via PayPal or Flattr:</p>
 ",
 'CONTRIBUTE_FANART'	    => '
     <h2>i lo se finti be lo nelci</h2>
-    <p>i ka\'e finti lo arte pe la\'e la piper jo\'u la karot: Drawing, scenarios, scupltures, 3D models, fan-fiction. Send them to me (<a href="mailto:info@davidrevoy.com">info@davidrevoy.com</a>, or poke me on social networks) to appear in the fan-art gallery:</p>
+    <p>i ka\'e finti lo arte pe la\'e la piper joi la karot: Drawing, scenarios, scupltures, 3D models, fan-fiction. Send them to me (<a href="mailto:info@davidrevoy.com">info@davidrevoy.com</a>, or poke me on social networks) to appear in the fan-art gallery:</p>
 ',
 'CONTRIBUTE_DERIVATIONS' => '
     <h2>lo se jicmu be lo se finti be mi</h2>
@@ -156,27 +156,27 @@ i sa'u do jmina lo notci be ro da poi finti gi'a fanva gi'a cikre vau lo se piln
 ',
 
 'CONTRIBUTE_BOTTOM'	    => '
-    <h2>.i nu fanva .i nu cikre</h2>
+    <h2>i nu fanva i nu cikre</h2>
 
     <p>Pepper&amp;Carrot website is designed to be multilingual and accept any language (including extinct ones, or fictive ones). The sources of this page are at your disposal for you to translate them. Check the <a href="index.php?fr/article267/translation-tutorial">reference tutorial</a> for more information on how to add your translation.</p>
 
-    <h2>.i karni .i tcana le nu jikca</h2>
+    <h2>i karni i tcana le nu jikca</h2>
 
     <p>Be the publisher of Pepper&amp;Carrot! Write articles, create posts on websites, share and build a community on your favorite social networks. You can download the <a href="?static6/sources" title="Sources page">press kit</a> on the "sources" page.</p>
 
     <h2>Other contributions ideas&nbsp;.&nbsp;.&nbsp;.&nbsp;</h2>
 
     <p>i ro prenu ka\'e sidju<br/>
-        <b>i lo jai gau farvi</b> cu go\'i fi lo ka proga be lo nu tcidu do\'e la piper jo\'u la karot bei do\'e lo cmalu skami<br/>
-        <b>i lo finti be lo zgike</b> cu ka\'e finti lo zgike poi srana py. jo\'u ky.<br/>
-        <b>i lo finti be lo prosa</b> ka\'e stidi tu\'a lo cnino lisri be py. jo\'u cy.<br/>
+        <b>i lo jai gau farvi</b> cu go\'i fi lo ka proga be lo nu tcidu do\'e la piper joi la karot bei do\'e lo cmalu skami<br/>
+        <b>i lo finti be lo zgike</b> cu ka\'e finti lo zgike poi srana py. joi ky.<br/>
+        <b>i lo finti be lo prosa</b> ka\'e stidi tu\'a lo cnino lisri be py. joi cy.<br/>
         <b>Journalist:</b> Inform about Pepper&amp;Carrot in traditionnal media (printed press, tv, etc.)<br/>
         <b>Printer:</b> Print posters or goodies with Pepper&amp;Carrot on it.<br/>
     </p>
 
     <h2>i di\'e se irci</h2>
 
-    <p>.i e\'e do irci gi\'e casnu la piper jo\'u la karot .i mi ta\'e jundi ca lo ropno donri i zoi zoi.deevad.zoi. cmene mi lo se irci i mi tavla bau lo fraos e lo glico<br/>
+    <p>i e\'e do irci gi\'e casnu la piper joi la karot i mi ta\'e jundi ca lo ropno donri i zoi zoi.deevad.zoi. cmene mi lo se irci i mi tavla bau lo fraos e lo glico<br/>
 ',
 
 ################################################################################
@@ -205,18 +205,18 @@ i sa'u do jmina lo notci be ro da poi finti gi'a fanva gi'a cikre vau lo se piln
 # Page: Author
 'AUTHOR_TITLE'		=> 'srana la\'o gy.David Revoy.gy.',
 'AUTHOR_BIO'	    => "
-    <p>coi do mi se cmene zoi gy.David Revoy.gy. gi'e fraso finti be lo pixra gi'e de'i li nanca bu 1981 co'a. .i mi ctuca mi gi'e nelci lo nu zbasu lo pixra vau e lo mlatu e lo skami e lo kulnu be lo pilno be la'o gy.Gnu/Linux.gy. noi lo jicmu be ke'a cu fingubni i ji'a mi nelci lo kibro e lo RPG laldo se kelci e lo laldo pixra je lisri e lo tcaci arte e lo kulnu be lo ponjo be'o e lo fantasi zei cfika li'o<br/>
+    <p>coi do mi se cmene zoi gy.David Revoy.gy. gi'e fraso finti be lo pixra gi'e de'i li nanca bu 1981 co'a i mi ctuca mi gi'e nelci lo nu zbasu lo pixra vau e lo mlatu e lo skami e lo kulnu be lo pilno be la'o gy.Gnu/Linux.gy. noi lo jicmu be ke'a cu fingubni i ji'a mi nelci lo kibro e lo RPG laldo se kelci e lo laldo pixra je lisri e lo tcaci arte e lo kulnu be lo ponjo be'o e lo fantasi zei cfika li'o<br/>
     <br/>
     After more than 10 years of Freelance in digital-painting, teaching, concept-art, illustration, art-direction, I decided to start my own project. I finally found a way to mix all my passions together; and the result is Pepper&amp;Carrot.</p>
 
-    <p>.i lo jai se gunka be mi</p>
+    <p>i lo jai se gunka be mi</p>
 ",
 'AUTHOR_TODO_DREAM'	=> '
     <h3>i mi djica tu\'a la\'e di\'e</h3>
 
     <p>
-    ☐ i tavla fi la piper e la karot au lo ka vitke fi lo te penmi be lo ponjo finti be lo pixra lisri<br/>
-    ☐ i da se skami se kelci  do\'e la piper jo\'u la karot<br/>
+    ☐ i tavla fi la piper e la karot fau le nu mi zvati le te penmi be lo ponjo finti be lo pixra lisri<br/>
+    ☐ i da se skami se kelci  do\'e la piper joi la karot<br/>
     ☐ i gasnu lo nu su\'o 100 pixra poi se finti lo drata co\'a zasti<br/>
     ☑ <a href="http://www.peppercarrot.com/en/article376/dream-to-do-list-a-wikipedia-page">i da papri do\'e la uikipediias</a><br/>
     ☐ i cpacu lo se kacma pixra be lo dasni bei tai tu\'a la piper<br/>
