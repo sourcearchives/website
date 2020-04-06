d7771d33000d14b5e84c88fc6862d0f467c21d9e
Steve Harris <steronydh@gmail.com>
2020-04-06 12:23:52 +0200

    Upload New File

---
+++ kw.php
@@ -0,0 +1,281 @@
+<?php
+
+$LANG = array(
+
+# Global
+'TRANSLATED_BY'              => 'Aswonans treylyans: Steve Harris',
+'LANGUAGE_NAME'              => 'Kernewek',
+'LANGUAGE_ISO_CODE_2_LETTER' => 'kw',
+# http://www.w3schools.com/tags/ref_language_codes.asp --> full list
+
+################################################################################
+# Header HTML infos for search engine and title in tab:
+'PEPPERCARROT_TITLE'    =>  'Pepper&amp;Carrot', // the title on top of the website
+'PEPPERCARROT_VEGETABLE' => 'Puber ha Karetysen', // literal translation of the spice and the vegetable
+'Website_DESCRIPTION'    => 'Gwisva sodhek a Pepper&amp;Carrot, roskomik rydh(libre) ha fenten ygor a-dro dhe Pepper, gwragh yowynk, ha hy hath, Carrot. Trigys esons yn ollvys fantasiek a ismek, pystri ha kroaduryon.',
+'SUBTITLE'               => 'An Roskomik Ygor gans David Revoy',
+
+################################################################################
+# Top menu website:
+'HOMEPAGE'   => 'Folen dre',
+'WEBCOMICS'  => 'Roskomigow',
+'BLOG'       => 'Blog',
+'PHILOSOPHY' => 'Filosofieth',
+'CONTRIBUTE' => 'Kevri',
+'COMMUNITY'  => 'Kemeneth',
+'WIKI'       => 'Wiki',
+'SOURCES'    => 'Fentynnyow',
+'AUTHOR'     => 'Awtour',
+'EXTRAS'     => 'Keworransow',
+'ESHOP'      => 'eGwerthji',
+'FOLLOW'     => 'Holya Pepper&amp;Carrot war:',
+
+# Top menu website buttons:
+'NAVIGATION_TRANSCRIPT' => 'Treusskrifans',
+'NAVIGATION_TRANSCRIPT_ON' => 'Klickya dhe dhiskwedhes treusskrifans',
+'NAVIGATION_TRANSCRIPT_OFF' => 'Klickya dhe gudha treusskrifans',
+'NAVIGATION_TRANSCRIPT_UNAVAILABLE' => 'Nyns eus treusskrifans rag an rann ma ha&#39;n yeth ma.',
+'NAVIGATION_HD' => 'HD 2400px',
+'NAVIGATION_HD_ON' => 'Klickya dhe dhiskwedhes imajys fin',
+'NAVIGATION_HD_OFF' => 'Klickya dhe dhiskwedhes imajys savonek',
+'NAVIGATION_DICTIONARY' => 'Gerlyver',
+'NAVIGATION_DICTIONARY_ALT' => 'Klickya dhe ygeri gans Wordlink rag hwithrans gerlyver uskis',
+
+################################################################################
+# Top and bottom translation panel
+'ADD_TRANSLATION' => 'keworra treylyans',
+'CORRECTIONS'     => 'profya ewnheans',
+
+################################################################################
+# Page: Homepage
+'HOMEPAGE_BIG_TEXT'         => '
+    Roskomik rydh(libre) ha fenten ygor<br/>
+    skoodhys heb mayner gans y dasogyon<br/>
+    rag chanjya an diwysyans lyver komik!<br/>
+',
+'HOMEPAGE_PATREON_INFO'     => 'Rag marnas $1 orth an rann nowydh, dos ha bos tasek hedhyw war Patreon',
+'HOMEPAGE_MOREINFO_BUTTON'  => 'Pella derivadow',
+'HOMEPAGE_PATREON_BUTTON'   => 'Dos ha bos tasek Pepper&amp;Carrot war Patreon',
+'PATRONAGE_BUTTON'          => 'Dos ha bos tasek Pepper&amp;Carrot',
+'HOMEPAGE_LAST_EPISODE'     => 'Diwettha rann',
+'HOMEPAGE_NEWS_UPDATE'      => 'Nowodhow ha nowydhheansow',
+'HOMEPAGE_MOREPOSTS_BUTTON' => 'Moy a bostys',
+'HOMEPAGE_MAINSERVICE_LINK' => 'https://www.patreon.com/join/davidrevoy?',
+'HOMEPAGE_SUPPORTED_BY'     => 'skoodhys gans tasogyon.',
+'HOMEPAGE_ALTERNATIVES'     => 'Ken dewisyow:',
+'HOMEPAGE_FANART'           =>  'Art skoodhyer',
+
+################################################################################
+# Page: Webcomics
+'WEBCOMIC_EPISODE'              => 'Rannow roskomik',
+'WEBCOMIC_MAKINGOF'             => 'Gwrians-a',
+'WEBCOMIC_MAKINGOF_DESCRIPTION' => '
+<p>Gwrians-a ha <a href="http://www.davidrevoy.com/categorie5/tutorials">dyskansow art</a> yw bonusys arbennek skoodhys gans ow <a href="https://www.patreon.com/join/davidrevoy?">Patreons &#39;gorwer&#39;</a></p>
+',
+'WEBCOMIC_ARTWORK'              => 'Mirva artweyth',
+'WEBCOMIC_SKETCHES'             => 'Linenansow',
+'WEBCOMIC_OLDER_COMICS'         => 'Roskomigow kottha gans an keth awtour',
+
+################################################################################
+# Page: Blog
+# when contents are not English (no need to warn user the content is english only here):
+#'LIMITATIONS' 	=> ' Content available in english only ',
+'LIMITATIONS' => '',
+'TRANSLATION_FALLBACK' => '(Treylyans ankavadow. Omskoodhya war Sowsnek)',
+
+################################################################################
+# Page: Philosophy
+'PHILOSOPHY_TOP'    => '
+ <h2>Supported by patrons</h2>
+
+    <p>Pepper&amp;Carrot project is only funded by its patrons, from all around the world.
+    Each patron sends a little money for each new episode published and gets a credit at the end of the new episode.
+    Thanks to this system, Pepper&amp;Carrot can stay independent and never have to resort to advertising or any marketing pollution.</p>
+',
+'DONATE_INTRO'    => '
+    <p>
+    It&#39;s easy to become the patron of Pepper&amp;Carrot for only $1 per new episode <a href="https://www.patreon.com/join/davidrevoy?">on Patreon</a>.
+    Patreon accepts credit cards from all around the world and also PayPal.
+    </p>
+',
+'PHILOSOPHY_BOTTOM' => '
+    <img alt="illustration representing patronage" src="0_sources/0ther/misc/low-res/2015-02-09_philosophy_01-support_by-David-Revoy.jpg">
+
+    <h2>100% free(libre), forever, no paywall</h2>
+
+    <p>All the content I produce about Pepper&amp;Carrot is on this website, free(libre) and available to everyone.
+    I respect all of you equally: with or without money. All special bonuses I make for my patrons are also posted here.
+    Pepper&amp;Carrot will never ask you to pay anything or to get a subscription to get access to new content.</p>
+
+        <img alt="Illustration representing paywall" src="0_sources/0ther/misc/low-res/2015-02-09_philosophy_03-paywall_by-David-Revoy.jpg">
+
+    <h2>Open-source and permissive</h2>
+
+    <p>I want to give people the right to share, use, build and even make money upon the work I&#39;ve created.
+    All pages, artworks and content were made with Free(Libre) Open-Sources Software on GNU/Linux, and all sources are on this website, &#39;Source&#39; menu.
+    Commercial usage, translations, fan-art, prints, movies, video-games, sharing, and reposts are encouraged.
+    You just need to give appropriate credit to the authors (artists, correctors, translators involved in the artwork you want to use), provide a link to the license, and indicate if changes were made. You may do so in any reasonable manner, but not in any way that suggests the authors endorse you or your use.</p>
+
+    <div class="button">
+        <a href="https://creativecommons.org/licenses/by/4.0/" title="For more information, read the Creative Commons Attribution 4.0">
+            License: Creative Commons Attribution 4.0
+        </a>
+    </div>
+
+        <img alt="Illustration representing open-source media" src="0_sources/0ther/misc/low-res/2015-02-09_philosophy_04-open-source_by-David-Revoy.jpg">
+
+    <h2>Quality entertainment for everyone, everywhere</h2>
+
+    <p>Pepper&amp;Carrot is a comedy/humor webcomic suited for everyone, every age.
+    No mature content, no violence. Free(libre) and open-source, Pepper&amp;Carrot is a proud example of how cool free-culture can be.
+    I focus a lot on quality, because free(libre) and open-source doesn&#39;t mean bad or amateur. Quite the contrary.</p>
+
+        <img alt="Illustration representing comic pages flying around the world" src="0_sources/0ther/misc/low-res/2015-02-09_philosophy_05-everyone_by-David-Revoy.jpg">
+
+    <h2>Let&#39;s change comic industry!</h2>
+
+    <p>
+      Without intermediary between artist and audience you pay less and I benefit more. You support me directly.
+      No publisher, distributor, marketing team or fashion police can force me to change Pepper&amp;Carrot to fit their vision of &#39;the market&#39;.
+      Why couldn&#39;t a single success &#39;snowball&#39; to a whole industry in crisis? We&#39;ll see…
+    </p>
+
+    <div class="button">
+      <a href="?static12/donate" title="For only $1 per new episode, become a patreon of Pepper&amp;Carrot">
+        Help me boost Pepper&amp;Carrot production today.
+      </a>
+    </div>
+
+    <img alt="Illustration: comic book industry vs. Patreon support" src="0_sources/0ther/misc/low-res/2015-02-09_philosophy_06-industry-change_by-David-Revoy.jpg">
+ ',
+
+################################################################################
+# Page: Contribute
+'CONTRIBUTE_TITLE'       => 'Contribute',
+'CONTRIBUTE_TOP'         => '
+    <p>Thanks to the <a href="?static6/sources" title="Sources page">open sources</a> and the <a href="http://creativecommons.org/licenses/by/4.0/">
+    creative commons license</a> you can contribute to Pepper&amp;Carrot in many ways:</p>
+',
+
+'CONTRIBUTE_FANART'      => '
+    <h2>Fan-art</h2>
+
+    <p>Pepper&amp;Carrot is open to fan-art: drawings, scenarios, sculptures, 3D models, fan-fiction. Send them to me (<a href="mailto:info@davidrevoy.com">info@davidrevoy.com</a>, or poke me on social networks) to appear in the fan-art gallery:</p>
+',
+'CONTRIBUTE_DERIVATIONS' => '
+    <h2>Derivatives</h2>
+
+    <p>Pepper&amp;Carrot can be adapted to many project or products, why not do your own or join an existing one?</p>
+',
+
+'CONTRIBUTE_TRANSLATION' => '
+    <h2>Translations and corrections</h2>
+
+    <p>Pepper&amp;Carrot website is designed to be multilingual and accept any language (including extinct ones, or fictive ones). The sources of this page are at your disposal for you to translate them. Check the <a href="?static14/documentation&page=010_Translate_the_comic">reference tutorial</a> for more information on how to add your translation.</p>
+',
+
+'CONTRIBUTE_SOCIAL'      => 'Social networks',
+
+'CONTRIBUTE_PRESS'       => '
+    <h2>Press</h2>
+
+    <p>Be the publisher of Pepper&amp;Carrot! Write articles, create posts on websites, share and build a community on your favorite social networks. You can download the <a href="?static6/sources" title="Sources page">press kit</a> on the "sources" page.</p>
+',
+
+'CONTRIBUTE_OTHER'       => '
+    <h2>Other ideas to contribute…</h2>
+
+    <p>Everyone can contribute in different ways:<br/>
+        <b>Developers:</b> Create an application to read Pepper&amp;Carrot on mobile device.<br/>
+        <b>Musician:</b> Create music themes for Pepper&amp;Carrot.<br/>
+        <b>Writer:</b> Propose new Pepper&amp;Carrot scenarios.<br/>
+        <b>Journalist:</b> Talk about Pepper&amp;Carrot in traditional media (printed press, tv, etc.)<br/>
+        <b>Printer:</b> Print posters or goodies with Pepper&amp;Carrot on it.<br/>
+    </p>
+',
+
+'CONTRIBUTE_IRC'         => '
+    <h2 id="irc">IRC channel</h2>
+
+    <p>Chat and discuss about Pepper&amp;Carrot. I&#39;m around at French day time (nickname: deevad)<br/>
+',
+
+################################################################################
+# Page: Sources
+'SOURCES_TITLE'     => 'Sources',
+'SOURCES_TOP'       => '
+    <p><b>Welcome to the sources download center!</b><br/>
+    Here you&#39;ll find the source files of Pepper&amp;Carrot and more.<br/>
+    All digital painting files here are compatible with the latest version of <a href="https://krita.org/">Krita</a>.</p>
+',
+'SOURCES_BOTTOM'    => '
+    <p>By downloading and working with those files you agree to respect the terms of the <br/>
+    <a href="http://creativecommons.org/licenses/by/4.0/">Creative Commons Attribution license</a>.
+    Check the README files in each project for more infos.</p>
+
+    <p><b>Code repositories for translations, scripts, the website, graphics, etc.:</b><br/></p>
+',
+'SOURCE_COVER'      => 'Cover images',
+'SOURCE_KRITA'      => 'Krita source files',
+'SOURCE_TRANSLATOR' => 'Translator\'s pack',
+'SOURCE_WEB'        => 'Web: <span class="sourceinfos">90ppi, lightweight jpg</span>',
+'SOURCE_PRINT'      => 'Print: <span class="sourceinfos">300ppi, lossless quality</span>',
+'SOURCE_MONTAGE'    => 'Montage: <span class="sourceinfos">all panels in a single page</span>',
+
+################################################################################
+# Page: Author
+'AUTHOR_TITLE'                       => 'About David Revoy',
+'AUTHOR_BIO'                         => '
+    <p>Hi, my name is David Revoy and I&#39;m a French artist born in 1981. I&#39;m self-taught and passionate about drawing, painting, cats, computers, Gnu/Linux open-source culture, Internet, old school RPG video-games, old mangas and anime, traditional art, Japanese culture, fantasy…<br/>
+    <br/>
+    After more than 10 years of freelance in digital painting, teaching, concept-art, illustrating and art-direction I decided to start my own project. I finally found a way to mix all my passions together, the result is Pepper&amp;Carrot.</p>
+
+    <p>My portfolio:</p>
+',
+'AUTHOR_TODO_DREAM'                  => '
+    <h3 id="dream">Dream TO-DO list</h3>
+
+    <p>
+    ☐ Give a talk in a Japanese comic convention about Pepper&amp;Carrot.<br/>
+    ☐ Play with a gamepad to a Pepper&amp;Carrot video-game.<br/>
+    ☑ <a href="http://www.peppercarrot.com/en/article457/dream-to-do-list-100-fan-arts"> Get a gallery of 100 fan-arts.</a><br/>
+    ☑ <a href="http://www.peppercarrot.com/en/article376/dream-to-do-list-a-wikipedia-page">Get a Wikipedia page.</a><br/>
+    ☑ <a href="http://www.peppercarrot.com/article387/cosplay-by-maria-and-ekaterina">Receive a photo of a Pepper cosplay.</a><br/>
+    ☑ <a href="http://www.peppercarrot.com/article304/dream-to-do-list-a-cat-named-carrot">Receive a photo of a red cat named Carrot.</a><br/>
+    ☑ <a href="http://www.peppercarrot.com/article302/peppercarrot-has-over-500-supporters">Be supported by 500 patreons.</a><br/>
+    ☐ Reach episode 100!<br/>
+    </p>
+',
+
+'AUTHOR_CARREER_TITLE'               => 'My career in 7 bubbles',
+'AUTHOR_CARREER_BUBBLE_DESCRIPTIONS' => '<p></p>',
+
+################################################################################
+# Website General: Footer
+'FOOTER_CONTENT' => '
+    <p>Webcomic, artworks and texts are licensed under a <a href="https://creativecommons.org/licenses/by/4.0/" title="For more information, read the Creative Commons Attribution 4.0">Creative Commons Attribution 4.0</a> license, unless otherwise noted in the page.<br/>
+    Attribution to "David Revoy, www.davidrevoy.com". Contact me: <a href="mailto:info@davidrevoy.com">info@davidrevoy.com</a> for more information.</p>
+
+    <p>Website powered by <a href="http://www.pluxml.org" title="PluXml">PluXml</a></p>
+',
+################################################################################
+# Eshop
+'ESHOP_COMIC'           => 'Yma lyvrow komik hag art warn:',
+'ESHOP_SHOP'            => 'Pryntys, hanafow, krysyow-T ha moy war:',
+################################################################################
+# various utils:
+'UTIL_NEXT_EPISODE'     => 'nessa rann',
+'UTIL_PREVIOUS_EPISODE' => 'rann kyns',
+'UTIL_EPISODE'          => 'rann',
+'UTIL_BACK_TO_GALLERY'  => 'dehweles dhe&#39;n virva',
+'UTIL_MORE'             => 'moy',
+'UTIL_PAGE'             => 'folen',
+'UTIL_BY'               => 'gans',
+'UTIL_LICENSE'          => 'leshyans',
+'UTIL_DOT'              => '.',
+'FIRST'                 => 'kynsa',
+'LAST'                  => 'diwettha',
+);
+
+# vim: set et ts=4 sw=4 :
+?>
